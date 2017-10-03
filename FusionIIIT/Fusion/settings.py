a4b43923dacd142aad2317bdf076fc9d97ac5a10
Arshad Siddiqui <arshad.sid97@gmail.com>
2017-10-04 01:59:40 +0530

    Changing DB
    DB has been changed to MySql, this will require you to create a DB at port 3306 with password and username root and root respectively. This can be done using MySql workbench and then creating a database schema named fusion. makemigrations and migrate after creating the database fusion for the app to work.

--- FusionIIIT/Fusion/settings.py
+++ FusionIIIT/Fusion/settings.py
@@ -88,9 +88,13 @@ WSGI_APPLICATION = 'Fusion.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'fusion',
+    'USER': 'root',
+    'PASSWORD': 'root',
+    'HOST': '127.0.0.1',
+    'PORT': '3306',
+},
 }
 
 
