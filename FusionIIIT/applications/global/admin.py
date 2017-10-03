df17ae67c1599ddabde3abe6c16517008fafc8f5
Arshad Siddiqui <arshad.sid97@gmail.com>
2017-10-03 16:13:20 +0530

    Adding global database models.

--- FusionIIIT/applications/global/admin.py
+++ FusionIIIT/applications/global/admin.py
@@ -1,3 +1,11 @@
-# from django.contrib import admin
+from django.contrib import admin
+from .models import Student, ExtraInfo, Staff, Faculty, DepartmentInfo, Designation
 
 # Register your models here.
+
+admin.site.register(Student)
+admin.site.register(ExtraInfo)
+admin.site.register(Staff)
+admin.site.register(Faculty)
+admin.site.register(DepartmentInfo)
+admin.site.register(Designation)
