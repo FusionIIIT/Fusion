77e40b030cf6ac50ef8b9608440069c1df53d498
Arshad Siddiqui <arshad.sid97@gmail.com>
2017-10-04 02:08:14 +0530

    flake8 compliant

--- FusionIIIT/Fusion/settings.py
+++ FusionIIIT/Fusion/settings.py
@@ -87,14 +87,15 @@ WSGI_APPLICATION = 'Fusion.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-    'ENGINE': 'django.db.backends.mysql',
-    'NAME': 'fusion',
-    'USER': 'root',
-    'PASSWORD': 'root',
-    'HOST': '127.0.0.1',
-    'PORT': '3306',
-},
+    'default':
+        {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'fusion',
+            'USER': 'root',
+            'PASSWORD': 'root',
+            'HOST': '127.0.0.1',
+            'PORT': '3306',
+        },
 }
 
 
