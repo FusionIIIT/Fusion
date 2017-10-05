4987dd6ad4f393d328750f7c4cea073e76629fb6
Zero_Bytes <sidd.arshad916@gmail.com>
2017-10-06 01:00:18 +0530

    Database (arshad) (#9)
    * Update README.md
    
    * Updated Readme file
    
    * Adding global database models.
    
    * Correcting the line spacing and spellings
    
    * Update README.md
    
    * More informative readme
    
    * Corrected using flake8 and isort
    
    * Flke8 and isort compliant
    
    * Adding EIS application
    
    adding the employee information system app to the applications folder
    
    * Changing DB
    
    DB has been changed to MySql, this will require you to create a DB at port 3306 with password and username root and root respectively. This can be done using MySql workbench and then creating a database schema named fusion. makemigrations and migrate after creating the database fusion for the app to work.
    
    * flake8 compliant
    
    * Update models.py

--- FusionIIIT/Fusion/settings.py
+++ FusionIIIT/Fusion/settings.py
@@ -87,10 +87,15 @@ WSGI_APPLICATION = 'Fusion.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default':
+        {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'fusion',
+            'USER': 'root',
+            'PASSWORD': 'root',
+            'HOST': '127.0.0.1',
+            'PORT': '3306',
+        },
 }
 
 
