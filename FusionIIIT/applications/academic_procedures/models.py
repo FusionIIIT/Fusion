3f1edabe5d1b2e66ca9f00e6b670087984b7ef7c
achint0987 <achintachu@gmail.com>
2017-10-10 02:58:12 +0530

    Register model created

--- FusionIIIT/applications/academic_procedures/models.py
+++ FusionIIIT/applications/academic_procedures/models.py
@@ -1,1 +1,15 @@
-# from django.db import models
+from django.db import models
+from applications.globals.models import Student
+from applications.academic_information.models import Course
+
+import datetime
+
+# Create your models here.
+
+class Register(models.Model):
+
+	r_id =  models.IntegerField(primary_key = True)
+	course_id = models.ForeignKey(Course)
+	year = models.IntegerField(max_length=4, default=datetime.datetime.now().year)
+	student_id = models.ForeignKey(Student)
+	
