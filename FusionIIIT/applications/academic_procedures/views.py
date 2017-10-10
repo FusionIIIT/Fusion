2adafc8a5bb1a4f4218575325e9eb1aca6f8274d
achint0987 <achintachu@gmail.com>
2017-10-10 22:26:55 +0530

    changes made in view module

--- FusionIIIT/applications/academic_procedures/views.py
+++ FusionIIIT/applications/academic_procedures/views.py
@@ -1,3 +1,19 @@
-# from django.shortcuts import render
-
+import os
+from django.shortcuts import render, HttpResponseRedirect
+from django.http import HttpResponse
+from . models import Resgister
+from applications.academic_information.models import (Calendar, Course, Exam_timetable, Grades, Holiday,
+                                                        Instructor, Meeting, Student_attendance, Timetable)
+from . forms import AddDropCourseForm 
 # Create your views here.
+
+
+@login_required(login_url='/login')
+def add_course(request):
+
+    if request.method == 'POST':
+        return HttpResponse('congratzzzzz')
+    else:
+        CourseForm = AddDropCourseForm(user=request.user)
+
+    return render(request, 'test.html', {'CourseForm': CourseForm})
