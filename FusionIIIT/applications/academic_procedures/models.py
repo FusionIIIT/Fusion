900204563bccb83e6c4b7c65c1451ebdb591c442
Anuraag <dbaanurag@gmail.com>
2017-10-10 17:14:04 +0530

    models updated

--- FusionIIIT/applications/academic_procedures/models.py
+++ FusionIIIT/applications/academic_procedures/models.py
@@ -3,7 +3,7 @@ import datetime
 from django.db import models
 
 from applications.academic_information.models import Course
-from applications.globals.models import Student
+from applications.globals.models import ExtraInfo, Faculty, Student
 
 # Create your models here.
 
@@ -11,7 +11,7 @@ from applications.globals.models import Student
 class Register(models.Model):
     r_id = models.IntegerField(primary_key=True)
     course_id = models.ForeignKey(Course)
-    year = models.IntegerField(max_length=4, default=datetime.datetime.now().year)
+    year = models.IntegerField(default=datetime.datetime.now().year)
     student_id = models.ForeignKey(Student)
 
     class Meta:
@@ -19,3 +19,16 @@ class Register(models.Model):
 
     def __str__(self):
         return self.r_id
+
+
+class Thesis(models.Model):
+    reg_id = models.ForeignKey(ExtraInfo, on_delete=models.CASCADE)
+    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+    supervisor_id = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+    topic = models.CharField(max_length=1000)
+
+    class Meta:
+        db_table = 'Thesis'
+
+    def __str__(self):
+        return self.topic & self.reg_id & self.student_id & self.supervisor_id
