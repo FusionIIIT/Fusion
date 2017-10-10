4629f818f1e8735f2d548d7b75b4b270791ef34a
parascoder1 <parascoder@gmail.com>
2017-10-11 02:34:15 +0530

    instructor view courses added

--- FusionIIIT/applications/online_cms/urls.py
+++ FusionIIIT/applications/online_cms/urls.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 from . import views
 urlpatterns = [
 
-    url(r'^viewcourses/$',views.viewcourses,name='viewcourses')
+    url(r'^viewcourses/$',views.viewcourses,name='viewcourses'),
+    url(r'^(?P<course_code>[A-z]+)/$',views.courses,name='courses')
+    # because course_name will have blank spaces in between which is not possible and i need to send something in the url so as to identify which course it is.
     #students
     #lecturer
 
--- FusionIIIT/applications/online_cms/views.py
+++ FusionIIIT/applications/online_cms/views.py
@@ -6,21 +6,28 @@ from applications.globals.models import *
 from applications.academic_procedures.models import *
 from applications.academic_information.models import *
 from datetime import datetime
+
 @login_required
 def viewcourses(request):
     user=request.user
     extrainfo=ExtraInfo.objects.get(user=user)
-    student=Student.objects.get(id=extrainfo)
-    month=datetime.now().month
-    sem=0
-    if month>=8 and month <=12:
-        sem=1
-    if month>=1 and month<8:
-        sem=2
-    roll=student.id.id[:4]
-    print(roll," ",student.id.user.username)
-    semester=(datetime.now().year-int(roll))*2+sem
-    print(semester,"sem")
-    register=Register.objects.filter(student_id=student,semester=semester)
+    if extrainfo.user_type == 'student':
+        student=Student.objects.get(id=extrainfo)
+        month=datetime.now().month
+        sem=0
+        if month>=8 and month <=12:
+            sem=1
+        roll=student.id.id[:4]
+        print(roll," ",student.id.user.username)
+        semester=(datetime.now().year-int(roll))*2+sem
+        print(semester,"sem")
+        register=Register.objects.filter(student_id=student,semester=semester)
+        return render(request,'online_cms/index.html',{'register':register})
+    else:
+        instructor=Instructor.objects.filter(instructor_id=extrainfo)
+        return render(request,'online_cms/index.html',{'instructor':instructor})
 
-    return render(request,'online_cms/index.html',{'register':register})
+
+@login_required
+def courses(request,course_code):
+    return HttpResponse(course_name)
