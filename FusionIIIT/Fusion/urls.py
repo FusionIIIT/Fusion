702afa36bed6df654222f76dab0d5e751a59ebc4
achint0987 <achintachu@gmail.com>
2017-10-10 23:17:17 +0530

    urls changed for login/logout

--- FusionIIIT/Fusion/urls.py
+++ FusionIIIT/Fusion/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^login/', auth_views.login, name='login'),
+    url(r'^logout/', auth_views.logout, name='logout'),
     url(r'^academic/', include('applications.academic_procedures.urls')),
 ]
