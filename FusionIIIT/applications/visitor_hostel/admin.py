5cf8cbab88619026ee0d2fae483ab732c4874f8e
anuj97 <anujupadhyayjnp@gmail.com>
2017-10-16 00:50:21 +0530

    models registered

--- FusionIIIT/applications/visitor_hostel/admin.py
+++ FusionIIIT/applications/visitor_hostel/admin.py
@@ -1,3 +1,11 @@
 # from django.contrib import admin
+from .models import (Visitor, Book_room, Visitor_bill, Room, Visitor_room, Meal, Inventory)
 
-# Register your models here.
+
+admin.site.register(Visitor)
+admin.site.register(Book_room)
+admin.site.register(Visitor_bill)
+admin.site.register(Room)
+admin.site.register(Visitor_room)
+admin.site.register(Meal)
+admin.site.register(Inventory)
