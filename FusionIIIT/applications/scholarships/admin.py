4a42db1d0f451ab985ada53a8abc636af2fb4d64
Pramod Kumar <pramod20gk@gmail.com>
2017-10-25 01:35:49 +0530

    added models.py and admin.py

--- FusionIIIT/applications/scholarships/admin.py
+++ FusionIIIT/applications/scholarships/admin.py
@@ -1,3 +1,16 @@
-# from django.contrib import admin
+from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+
+admin.site.register(Mcm)
+admin.site.register(Award_and_scholarship)
+admin.site.register(Previous_winner)
+admin.site.register(Release)
+admin.site.register(Financial_assistance)
+admin.site.register(Common_info)
+admin.site.register(Proficiency_dm)
+admin.site.register(Group_student)
+admin.site.register(Director_silver)
+admin.site.register(Director_gold)
