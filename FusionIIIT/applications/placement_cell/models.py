a327796950537dd557b41edd5d87cb23557bd90e
Gautam Yadav <gautamyadav@iiitdmj.ac.in>
2017-10-26 23:22:01 +0530

    Minor Changes

--- FusionIIIT/applications/placement_cell/models.py
+++ FusionIIIT/applications/placement_cell/models.py
@@ -33,6 +33,7 @@ class Constants:
         ('ON CAMPUS', 'On Campus'),
         ('PPO', 'PPO'),
         ('OFF CAMPUS', 'Off Campus'),
+        ('NOT PLACED', 'Not Placed')
     )
     DEBAR_TYPE = (
         ('DEBAR', 'Debar'),
@@ -290,11 +291,11 @@ class StudentPlacement(models.Model):
     future_aspect = models.CharField(max_length=20, choices=Constants.PLACEMENT_TYPE,
                                      default='PLACEMENT')
     placed_type = models.CharField(max_length=20, choices=Constants.PLACED_TYPE,
-                                   default='', null=True, blank=True)
+                                   default='NOT PLACED')
     placement_date = models.DateField(_("Date"), default=datetime.date.today, null=True,
                                       blank=True)
     package = models.DecimalField(default='', decimal_places=2, max_digits=5, null=True,
                                   blank=True)
 
     def __str__(self):
-        return self.unique_id.id
+        return self.unique_id.id.id
--- FusionIIIT/applications/placement_cell/views.py
+++ FusionIIIT/applications/placement_cell/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.db.models import Q
+from applications.globals.models import ExtraInfo
+from applications.academic_information.models import Student
+from .models import (Achievement, ChairmanVisit, Coauthor, Coinventor,
+                     ContactCompany, Course, Education, Experience, Has,
+                     Interest, Know, Language, MessageOfficer, NotifyStudent,
+                     Patent, PlacementRecord, PlacementSchedule,
+                     PlacementStatus, Project, Publication, Skill,
+                     StudentPlacement, StudentRecord)
 
 
 def placement(request):
     context = {}
 
+    return render(request, "placementModule/placement.html", context)
+
+def profile(request, username):
+    user = get_object_or_404(User, Q(username = username))
+    profile = get_object_or_404(ExtraInfo, Q(user = user))
+    student = get_object_or_404(Student, Q(id = profile.id))
+    studentplacement = get_object_or_404(StudentPlacement, Q(unique_id = student))
+    skills = Has.objects.filter(Q(unique_id = student))
+    context ={'user': user, 'profile': profile, 'student': studentplacement, 'skills': skills}
     return render(request, "placementModule/placement.html", context)
