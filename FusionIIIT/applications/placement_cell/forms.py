646a8bac75fc24ef8ee710c5a0a50b884de39596
Gautam Yadav <gautamyadav@iiitdmj.ac.in>
2017-10-28 01:08:56 +0530

    Added forms.py

---
+++ FusionIIIT/applications/placement_cell/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.contrib.admin import widgets
+from django.utils.translation import ugettext_lazy as _
+
+from .models import Education
+
+class AddEducation(forms.Form):
+
+    institute = forms.CharField(widget=forms.TextInput(attrs={'max_length': 250,
+                                                              'class': 'field'}),
+                           label="institute")
+    degree = forms.CharField(widget=forms.TextInput(attrs={'max_length': 40,
+                                                           'class': 'field'}),
+                            label="degree")
+    grade = forms.CharField(widget=forms.TextInput(attrs={'max_length': 10,
+                                                           'class': 'form-control'}),
+                            label="grade")
+    stream = forms.CharField(widget=forms.TextInput(attrs={'max_length': 150,
+                                                           'class': 'form-control'}),
+                            label="stream")
+    sdate = forms.DateField(label='sdate', widget=forms.widgets.DateInput())
+    edate = forms.DateField(label='edate', widget=forms.widgets.DateInput())
--- FusionIIIT/applications/placement_cell/views.py
+++ FusionIIIT/applications/placement_cell/views.py
@@ -4,7 +4,7 @@ from io import BytesIO
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.db.models import Q
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, render
 from django.template.loader import get_template
 
@@ -12,6 +12,7 @@ from applications.academic_information.models import Student
 from applications.globals.models import ExtraInfo
 from xhtml2pdf import pisa
 
+from .forms import AddEducation
 from .models import (Achievement, Course, Education, Experience, Has, Project,
                      Publication, StudentPlacement)
 
@@ -25,21 +26,41 @@ def placement(request):
 
 @login_required
 def profile(request, username):
-    user = get_object_or_404(User, Q(username=username))
-    profile = get_object_or_404(ExtraInfo, Q(user=user))
-    student = get_object_or_404(Student, Q(id=profile.id))
-    studentplacement = get_object_or_404(StudentPlacement, Q(unique_id=student))
-    skills = Has.objects.filter(Q(unique_id=student))
-    education = Education.objects.filter(Q(unique_id=student))
-    course = Course.objects.filter(Q(unique_id=student))
-    experience = Experience.objects.filter(Q(unique_id=student))
-    project = Project.objects.filter(Q(unique_id=student))
-    achievement = Achievement.objects.filter(Q(unique_id=student))
-    publication = Publication.objects.filter(Q(unique_id=student))
-    context = {'user': user, 'profile': profile, 'student': studentplacement, 'skills': skills,
-               'educations': education, 'courses': course, 'experiences': experience,
-               'projects': project, 'achievements': achievement, 'publications': publication}
-    return render(request, "placementModule/placement.html", context)
+    if request.method == 'POST':
+        form = AddEducation(request.POST)
+        user = get_object_or_404(User, Q(username=username))
+        profile = get_object_or_404(ExtraInfo, Q(user=user))
+        student = get_object_or_404(Student, Q(id=profile.id))
+        if form.is_valid():
+            institute = form.cleaned_data['institute']
+            degree = form.cleaned_data['degree']
+            grade = form.cleaned_data['grade']
+            stream = form.cleaned_data['stream']
+            sdate = form.cleaned_data['sdate']
+            edate = form.cleaned_data['edate']
+            education_obj = Education.objects.create(unique_id = student, degree = degree,
+                                                     grade = grade, institute = institute,
+                                                     stream = stream, sdate = sdate, edate = edate)
+            education_obj.save()
+            return render(request, "placementModule/placement.html", context)
+    else:
+        form = AddEducation(initial={})
+        user = get_object_or_404(User, Q(username=username))
+        profile = get_object_or_404(ExtraInfo, Q(user=user))
+        student = get_object_or_404(Student, Q(id=profile.id))
+        studentplacement = get_object_or_404(StudentPlacement, Q(unique_id=student))
+        skills = Has.objects.filter(Q(unique_id=student))
+        education = Education.objects.filter(Q(unique_id=student))
+        course = Course.objects.filter(Q(unique_id=student))
+        experience = Experience.objects.filter(Q(unique_id=student))
+        project = Project.objects.filter(Q(unique_id=student))
+        achievement = Achievement.objects.filter(Q(unique_id=student))
+        publication = Publication.objects.filter(Q(unique_id=student))
+        context = {'user': user, 'profile': profile, 'student': studentplacement, 'skills': skills,
+                   'educations': education, 'courses': course, 'experiences': experience,
+                   'projects': project, 'achievements': achievement, 'publications': publication,
+                   'form': form}
+        return render(request, "placementModule/placement.html", context)
 
 
 def render_to_pdf(template_src, context_dict):
@@ -65,7 +86,7 @@ def cv(request, username):
     project = Project.objects.filter(Q(unique_id=student))
     achievement = Achievement.objects.filter(Q(unique_id=student))
     publication = Publication.objects.filter(Q(unique_id=student))
-    return render_to_pdf('placementModule/placement.html', {'pagesize': 'A4', 'user': user,
+    return render_to_pdf('placementModule/cv.html', {'pagesize': 'A4', 'user': user,
                                                             'profile': profile,
                                                             'student': studentplacement,
                                                             'skills': skills,
