a90a5c8f8550ec760598427bf4b6962a7ff422f5
Samy-33 <saket.is.sam@gmail.com>
2017-11-09 21:47:33 +0530

    changed db back to sqlite

--- FusionIIIT/Fusion/settings.py
+++ FusionIIIT/Fusion/settings.py
@@ -146,16 +146,24 @@ WSGI_APPLICATION = 'Fusion.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default':
+#         {
+#             'ENGINE': 'django.db.backends.mysql',
+#             'NAME': 'fusion',
+#             'USER': 'root',
+#             'PASSWORD': '',
+#             'HOST': '127.0.0.1',
+#             'PORT': '3306',
+#         },
+# }
+
+# We will use sqlite for development, so we don't waste our time resolving mysql errors.
 DATABASES = {
-    'default':
-        {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'fusion',
-            'USER': 'root',
-            'PASSWORD': '',
-            'HOST': '127.0.0.1',
-            'PORT': '3306',
-        },
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
