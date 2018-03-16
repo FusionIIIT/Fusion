000d86998f8a01a9f15d2faaf06cf4eeaca63844
Samy-33 <saket.is.sam@gmail.com>
2018-03-16 13:03:55 +0530

    isort errors and version update

--- FusionIIIT/Fusion/urls.py
+++ FusionIIIT/Fusion/urls.py
@@ -1,10 +1,8 @@
 from django.conf import settings
 from django.conf.urls import include, url
-
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-
 
 urlpatterns = [
     url(r'^', include('applications.globals.urls')),
