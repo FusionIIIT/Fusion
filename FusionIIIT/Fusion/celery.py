62709484119fc9ff194ddb3b8e7b96fa4e95a6bc
Samy-33 <saket.is.sam@gmail.com>
2018-03-30 19:54:40 +0530

    isort version restored

--- FusionIIIT/Fusion/celery.py
+++ FusionIIIT/Fusion/celery.py
@@ -2,9 +2,8 @@ from __future__ import absolute_import
 
 import os
 
-from django.conf import settings
-
 from celery import Celery
+from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Fusion.settings')
