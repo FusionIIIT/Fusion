72a092e98f81f37d04ff97f24da613869d65f2c7
Pratyush2710 <pratyushhg@gmail.com>
2019-01-21 22:53:06 +0530

    bug fixed

--- FusionIIIT/applications/office_module/views_office_students.py
+++ FusionIIIT/applications/office_module/views_office_students.py
@@ -1,4 +1,4 @@
-git
+
 from django.shortcuts import render , get_object_or_404
 from django.http import HttpResponse , HttpResponseRedirect
 from applications.academic_information.models import Meeting
