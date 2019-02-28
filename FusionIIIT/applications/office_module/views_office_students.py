64f7b511e83f9ba6145aa76b69ef473af1de3b07
Somya <jainsomya972@gmail.com>
2019-02-28 21:10:55 +0530

    code cleaning

--- FusionIIIT/applications/office_module/views_office_students.py
+++ FusionIIIT/applications/office_module/views_office_students.py
@@ -8,20 +8,39 @@ from applications.globals.models import *
 import json
 from django.contrib.auth.decorators import login_required
 
+"""
+    Default view for Dean Students Module
+    All models are Loaded from respective Tables in Database and passed to default template
+    List of Concerned Models :
+    S.no. Model             |  Load Variables  |  Usage
+    
+    1.    Club_budget       |  budget_app      |  Budget reuests where status is open
+                            |  past_budget     |  Past budget requests
+                            |  approved_budgets|  Budget requests marked as approved
+    2.    Meeting           |  minutes         |  Meeting entries where minutes file is not present
+                            |  final_minutes   |  Meeting entries where minute file is present
+    3.    hostel_allotment  |  hall_allotment  |  All entries
+    4.    Club_info         |  clubNew         |  Clubs which are not approved/rejected
+                            |  club            |  Clubs which are approved/rejected
+                            |  budgets         |  Clubs which are approved
+    5.    HoldsDesignation  |  designation     |  Designation of the active user
+"""
 @login_required
 def officeOfDeanStudents(request):
-    budget_app= Club_budget.objects.all().filter(status='open');
-    past_budget=Club_budget.objects.all().exclude(status='open');
-    minutes=Meeting.objects.all().filter(minutes_file="");
-    final_minutes=Meeting.objects.all().exclude(minutes_file="");
-    hall_allotment=hostel_allotment.objects.all()
-    clubNew= Club_info.objects.all().filter(status='open')
-    club =Club_info.objects.all().exclude(status='open')
-    budgets=Club_info.objects.all().filter(status='confirmed')
-    approved_budgets=Club_budget.objects.all().filter(status='confirmed')
-    budget_allotment= Club_info.objects.all().filter(status='confirmed' , alloted_budget=0)
+    budget_app = Club_budget.objects.all().filter(status='open')
+    past_budget = Club_budget.objects.all().exclude(status='open')
+    minutes = Meeting.objects.all().filter(minutes_file="")
+    final_minutes = Meeting.objects.all().exclude(minutes_file="")
+    hall_allotment = hostel_allotment.objects.all()
+    clubNew = Club_info.objects.all().filter(status='open')
+    club = Club_info.objects.all().exclude(status='open')
+    budgets = Club_info.objects.all().filter(status='confirmed')
+    approved_budgets = Club_budget.objects.all().filter(status='confirmed')
+    budget_allotment = Club_info.objects.all().filter(status='confirmed')
     budget_alloted = Club_info.objects.all().exclude(alloted_budget=0)
     designation = HoldsDesignation.objects.all().filter(working=request.user)
+
+    """getting roll and designation(s) of the active user in roll_"""
     desig = list(HoldsDesignation.objects.all().filter(working = request.user).values_list('designation'))
     b = [i for sub in desig for i in sub]
     roll_=[]
@@ -29,30 +48,26 @@ def officeOfDeanStudents(request):
         name_ = get_object_or_404(Designation, id = i)
         roll_.append(str(name_.name))
 
-    all_designation=[]
-    for i in designation:
-        all_designation.append(str(i.designation))
     HALL_NO = (('HALL-1','hall-1'),('HALL-3','hall-3'),('HALL-4','hall-4'))
     hall=[]
-    hostel_file= hostel_allotment.objects.all()
     for i in HALL_NO:
-        if(str(i[1]) ):  #not in [j.hall_no for j in hostel_file]#
+        if(str(i[1])):
             hall.append(str(i[1]))
-    print(roll_)
-    context = {'meetingMinutes':minutes,
-                'final_minutes':final_minutes,
+
+    context = {'meetingMinutes': minutes,
+                'final_minutes': final_minutes,
                 'hall': hall,
-                'hall_allotment':hall_allotment,
-                'budget_app':budget_app,
-                'p_budget':past_budget,
-                'clubNew':clubNew,
-                'club':club,
-                'budgets':budgets,
-                'approved_budgets':approved_budgets,
-                'budget_allotment':budget_allotment,
-                'budget_alloted' : budget_alloted,
-                'all_designation' : roll_,
-                }
+                'hall_allotment': hall_allotment,
+                'budget_app': budget_app,
+                'p_budget': past_budget,
+                'clubNew': clubNew,
+                'club': club,
+                'budgets': budgets,
+                'approved_budgets': approved_budgets,
+                'budget_allotment': budget_allotment,
+                'budget_alloted': budget_alloted,
+                'all_designation': roll_,
+              }
     return render(request, "officeModule/officeOfDeanStudents/officeOfDeanStudents.html", context)
 
 @login_required
@@ -92,7 +107,7 @@ def budgetApproval(request):
     id_r=request.POST.getlist('check')
     remark=request.POST.getlist('remark')
     for i in range(len(id_r)):
-        a=Club_budget.objects.get(id=id_r[i]);
+        a=Club_budget.objects.get(id=id_r[i])
         a.status='confirmed'
         a.remarks=request.POST.get(id_r[i])
         budget= request.POST.get('amount '+ id_r[i])
