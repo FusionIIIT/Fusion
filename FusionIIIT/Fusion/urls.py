3f5ca5429837d4e66286c81390793243d4d45ccf
Abhay Gupta <abhay.abhaygupta.gupta3@gmail.com>
2019-04-01 21:12:30 +0530

    Leave updated and some settings change

--- FusionIIIT/Fusion/urls.py
+++ FusionIIIT/Fusion/urls.py
@@ -51,6 +51,8 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
 
     import debug_toolbar
     urlpatterns = [
