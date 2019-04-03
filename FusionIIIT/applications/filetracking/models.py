a918a29ba5b1354b6a98476a4be6aa3848e1b9c7
Kushdeep <rathujj123@gmail.com>
2019-04-03 12:23:18 +0530

    first commmit

--- FusionIIIT/applications/filetracking/models.py
+++ FusionIIIT/applications/filetracking/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from applications.globals.models import ExtraInfo, HoldsDesignation, Designation
 
 
@@ -10,6 +11,8 @@ class File(models.Model):
     description = models.CharField(max_length=400, null=True, blank=True)
     upload_date = models.DateTimeField(auto_now_add=True)
     upload_file = models.FileField(blank=True)
+    is_read = models.BooleanField(default = False)
+
 
     class Meta:
         db_table = 'File'
@@ -22,8 +25,11 @@ class Tracking(models.Model):
     file_id = models.ForeignKey(File, on_delete=models.CASCADE, null=True)
     current_id = models.ForeignKey(ExtraInfo, on_delete=models.CASCADE)
     current_design = models.ForeignKey(HoldsDesignation, null=True, on_delete=models.CASCADE)
-    receiver_id = models.ForeignKey(ExtraInfo, on_delete=models.CASCADE, related_name='receiver_id')
-    receive_design = models.ForeignKey(HoldsDesignation, null=True, on_delete=models.CASCADE, related_name='rec_design')
+        # receiver_id = models.ForeignKey(ExtraInfo, on_delete=models.CASCADE, related_name='receiver_id')
+    # receive_design = models.ForeignKey(HoldsDesignation, null=True, on_delete=models.CASCADE, related_name='rec_design')
+    receiver_id = models.ForeignKey(User,null = True, on_delete=models.CASCADE, related_name='receiver_id')
+    receive_design = models.ForeignKey(Designation, null=True, on_delete=models.CASCADE, related_name='rec_design')
+
     receive_date = models.DateTimeField(auto_now_add=True)
     forward_date = models.DateTimeField(auto_now_add=True)
     remarks = models.CharField(max_length=250, null=True, blank=True)
