dc1173186cb59efefd792099a83efee027095eb7
Abhay Gupta <abhay.abhaygupta.gupta3@gmail.com>
2019-04-05 14:56:44 +0530

    complaint module running

--- FusionIIIT/applications/placement_cell/views.py
+++ FusionIIIT/applications/placement_cell/views.py
@@ -2620,7 +2620,7 @@ def render_to_pdf(template_src, context_dict):
     template = get_template(template_src)
     html = template.render(context_dict)
     result = BytesIO()
-    pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)
+    pdf = pisa.pisaDocument(BytesIO(html.encode("UTF-8")), result)
     if not pdf.err:
         return HttpResponse(result.getvalue(), content_type='application/pdf')
     return HttpResponse('We had some errors<pre>%s</pre>' % escape(html))
