92f504ebaf461c49d6f53bb671c5bbc316cc7ba3
Manav <manavgoyal53@gmail.com>
2019-09-12 14:04:39 +0530

    Database ka rona khatm

--- FusionIIIT/makemigrations.sh
+++ FusionIIIT/makemigrations.sh
@@ -1,11 +1,9 @@
 python manage.py makemigrations globals
-python manage.py makemigrations leave
 python manage.py makemigrations eis
 python manage.py makemigrations academic_information
 python manage.py makemigrations academic_procedures
 python manage.py makemigrations library
 python manage.py makemigrations gymkhana
-python manage.py makemigrations office_module
 python manage.py makemigrations central_mess
 python manage.py makemigrations complaint_system
 python manage.py makemigrations filetracking
@@ -19,3 +17,5 @@ python manage.py makemigrations visitor_hostel
 python manage.py makemigrations allauth
 python manage.py makemigrations pagedown
 python manage.py makemigrations markdown_deux
+python manage.py makemigrations leave
+python manage.py makemigrations office_module
