6126690f019548d5ea6669bc464e6ce33ee54abd
Manav <manavgoyal53@gmail.com>
2019-09-12 16:56:00 +0530

    Ek  baar phir se.....

--- FusionIIIT/makemigrations.sh
+++ FusionIIIT/makemigrations.sh
@@ -1,9 +1,11 @@
 python manage.py makemigrations globals
+python manage.py makemigrations leave
 python manage.py makemigrations eis
 python manage.py makemigrations academic_information
 python manage.py makemigrations academic_procedures
 python manage.py makemigrations library
 python manage.py makemigrations gymkhana
+python manage.py makemigrations office_module
 python manage.py makemigrations central_mess
 python manage.py makemigrations complaint_system
 python manage.py makemigrations filetracking
@@ -17,5 +19,3 @@ python manage.py makemigrations visitor_hostel
 python manage.py makemigrations allauth
 python manage.py makemigrations pagedown
 python manage.py makemigrations markdown_deux
-python manage.py makemigrations leave
-python manage.py makemigrations office_module
