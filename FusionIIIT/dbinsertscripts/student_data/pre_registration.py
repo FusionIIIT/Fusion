a45529f3e3ef85d7e5b2c231fcb00015a460601a
salarx <2017222@iiitdmj.ac.in>
2019-11-13 00:21:39 +0530

    zLJd;al

--- FusionIIIT/dbinsertscripts/student_data/pre_registration.py
+++ FusionIIIT/dbinsertscripts/student_data/pre_registration.py
@@ -27,12 +27,15 @@ for filename in filenames:
     sem = (10-int(filename[5:6]))*2
     credits = 2
     for i in range(1,number_rows):
-        username = str(z.cell(i, 1).value).strip()
+        username = int(z.cell(i, 1).value)
+        username = str(username).strip()
+        print(username)
         user = User.objects.get(username=username)
         ext = ExtraInfo.objects.all().filter(user=user).first()
-        st = Student.objects.all().filter(id=ext.id).first()
+        st = Student.objects.all().filter(id=ext).first()
         for j in range(4,number_cols):
             course_name = str(z.cell(i, j).value).strip()
+            print(course_name)
             if(course_name!=''):
                 try:
                     course_obj = Course.objects.get(course_name = course_name)
@@ -40,29 +43,30 @@ for filename in filenames:
                     course_obj = Course(course_name = course_name)
                     course_obj.save()
                 try:
-                    curriculum_obj = Curriculum.objects.get(course_id = course_obj)
+                    print(1)
+                    curriculum_obj = Curriculum.objects.filter(course_id__course_name = course_name).first()
+                    print(2)
                 except:
-                    curriculum_obj = Curriculum.objects(
+                    curriculum_obj = Curriculum(
                         course_id = course_obj,
                         credits = credits,
                         course_type = 'Professional Core',
                         programme = programme,
                         batch = batch,
                         sem = sem,
-                        floated = True
+                        floated = True)
+                    curriculum_obj.save()
+                p = InitialRegistrations(
+                    curr_id = curriculum_obj,
+                    semester = sem,
+                    student_id = st,
+                    batch = batch
                     )
-                try:
-                    p = InitialRegistrations(
-                        curr_id = curriculum_obj,
-                        semester = sem,
-                        student_id = st,
-                        batch = batch
+                p.save()
+                check = StudentRegistrationCheck(
+                            student = st,
+                            pre_registration_flag = True,
+                            final_registration_flag = False,
+                            semester = sem
                         )
-                    p.save()
-                    check = StudentRegistrationCheck(
-                                student = current_user,
-                                pre_registration_flag = True,
-                                final_registration_flag = False,
-                                semester = sem
-                            )
-                    check.save()
+                check.save()
