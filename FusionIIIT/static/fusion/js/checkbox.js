7f10b745c9feaaffe4aead00734171f3be1e84ca
salarx <2017222@iiitdmj.ac.in>
2019-11-16 17:20:50 +0530

    removed static

--- FusionIIIT/static/fusion/js/checkbox.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-    $('.ui.checkbox')
-        .checkbox()
-    ;
-});
--- FusionIIIT/static/globals/js/datepicker.4fdb1208d573.js
+++
@@ -1,8 +0,0 @@
-$('.rangestart').calendar({
-    type: 'date',
-});
-$('.rangeend').calendar({
-    type: 'date',
-});
-
-$(".date.calendar").calendar({ type: "date" });
