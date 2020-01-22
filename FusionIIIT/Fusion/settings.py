a51e3e063e022ad263c9b6f440fccef7b4d59a18
salarx <2017222@iiitdmj.ac.in>
2020-01-22 13:57:02 +0530

    DB changes

--- FusionIIIT/Fusion/settings.py
+++ FusionIIIT/Fusion/settings.py
@@ -193,10 +193,10 @@ PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'fusionlab',
+        'NAME': 'fusion',
         'HOST': '172.27.16.216',
-        'USER': 'labuser',
-        'PASSWORD': 'secret123',
+        'USER': 'admin',
+        'PASSWORD': 'asd123asd',
     }
 }
 
