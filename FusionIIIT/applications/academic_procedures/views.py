269b345633cb6c59c6151b0d3ee2d6cfe063bc23
anshuman16423 <38358951+anshuman16423@users.noreply.github.com>
2020-03-03 17:44:07 +0530

    attendance fetch

--- FusionIIIT/applications/academic_procedures/views.py
+++ FusionIIIT/applications/academic_procedures/views.py
@@ -18,7 +18,8 @@ from django.shortcuts import get_object_or_404, render
 from django.template.loader import render_to_string
 from django.core.exceptions import ObjectDoesNotExist
 
-from applications.academic_information.models import Calendar, Course, Student,Curriculum_Instructor, Curriculum
+from applications.academic_information.models import (Calendar, Course, Student,Curriculum_Instructor, Curriculum,
+                                                      Student_attendance )
 from applications.globals.models import (DepartmentInfo, Designation,
                                          ExtraInfo, Faculty, HoldsDesignation)
 
@@ -207,8 +208,9 @@ def academic_procedures_student(request):
         current_date = demo_date.date()
         year = demo_date.year
         
-        registers = get_student_register(user_details.id)   
-        user_sem = get_user_semester(request.user, ug_flag, masters_flag, phd_flag)      
+        registers = get_student_register(user_details.id)
+        print(registers)
+        user_sem = get_user_semester(request.user, ug_flag, masters_flag, phd_flag)
         user_branch = get_user_branch(user_details)
         student_registration_check_pre = get_student_registrtion_check(obj,user_sem+1)
         student_registration_check_final = get_student_registrtion_check(obj,user_sem)
@@ -350,6 +352,23 @@ def academic_procedures_student(request):
                 print("entry in DB not found for student")
 
         tot_d = lib_d + acad_d + pc_d + hos_d + mess_d
+        obj = Student.objects.get(id=request.user.username)
+        course_list = []
+        for i in registers:
+            course_list.append(i.curr_id)
+        attendence = []
+        for i in course_list:
+
+            instructors = Curriculum_Instructor.objects.filter(curriculum_id=i)
+            pr,ab=0,0
+            for j in list(instructors):
+
+                presents = Student_attendance.objects.filter(student_id=obj,instructor_id=j, present=True)
+                absents = Student_attendance.objects.filter(student_id=obj,instructor_id=j, present=False)
+                pr += len(presents)
+                ab += len(absents)
+            attendence.append((i,pr,ab))
+        print(attendence)
         return render(
                           request, '../templates/academic_procedures/academic.html',
                           {'details': details,
@@ -400,6 +419,7 @@ def academic_procedures_student(request):
                            'pc_d':pc_d,
                            'hos_d':hos_d,
                             'tot_d':tot_d,
+                           'attendence':attendence,
                            }
                 )
 
