2037058205c1a297bf5f50b482a8e8731546e3fb
D. Debnath <d_debnath@outlook.com>
2020-05-30 16:47:06 +0530

    Integrated code of estate_module

--- FusionIIIT/applications/estate_module/views.py
+++ FusionIIIT/applications/estate_module/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required(login_url='/accounts/login/')
+def estate(request):
+
+    return render(request, "estate/home.html")
