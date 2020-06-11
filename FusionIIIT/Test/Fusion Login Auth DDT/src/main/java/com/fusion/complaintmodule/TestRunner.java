4101677ba068a70c3f977f65b60a24de488bf194
Sachin <2017219@iiitdj.ac.in>
2020-06-11 22:41:33 +0530

    DDT testing for Login Authentication
    Added Gherkin Script,Test Runner,Step Definations

---
+++ FusionIIIT/Test/Fusion Login Auth DDT/src/main/java/com/fusion/complaintmodule/TestRunner.java
@@ -0,0 +1,10 @@
+package com.fusion.complaintmodule;
+
+
+import cucumber.api.CucumberOptions;
+
+
+@CucumberOptions(features="/src/main/resources/main.feature",glue="com.fusion.complaintmodule")
+public class TestRunner{
+	
+}
