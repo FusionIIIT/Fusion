92ba4fa1a17359c0012b784d87e36eaba2ff873a
pragya <pragya@innovaccer.com>
2020-06-13 01:08:47 +0530

    added defination for lodging of complaint

--- Test/Fusion Login Auth DDT/src/main/java/com/fusion/complaintmodule/Step_Defination.java
+++ Test/Fusion Login Auth DDT/src/main/java/com/fusion/complaintmodule/Step_Defination.java
@@ -3,7 +3,9 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 
 import cucumber.api.java.en.Given;
@@ -51,6 +53,41 @@ public class Step_Defination {
 		String ExpectedTitle = "Fusion! Dashboard";
 		Assert.assertEquals(ActualTitle, ExpectedTitle);
 	    
+	}
+	@When("^click on complaint Module$")
+	public void Verify_Complaint_Module() throws Throwable {
+		driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[4]/div/div/div/div/div/div/div[4]/div[2]/a/div/div[2]/span")).click();
+		String ActualTitle = driver.getTitle();
+		String ExpectedTitle = "Fusion! Complaint";
+		Assert.assertEquals(ActualTitle, ExpectedTitle);
+	    
+	}
+	@Then("^Fill the form and select the Electricity as complaint type and Location as Hall3\r\n" + 
+			"$")
+	public void Verify_Electricity_And_Hall3_Combination() throws Throwable {
+		//Instantiate Action Class        
+        Actions actions = new Actions(driver);
+        //Retrieve WebElement 'Complaint Type' to perform mouse hover 
+    	WebElement Complaint_Type = driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[3]/div[1]/div[2]/div/form/div[2]/div[1]/div"));
+    	//Mouse hover menuOption 'Complaint Type'
+    	actions.moveToElement(Complaint_Type).perform();
+    	//Now Select 'Electricity' from sub menu which has got displayed on mouse hover of 'Complaint Type'
+    	WebElement subMenuOption = driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[3]/div[1]/div[2]/div/form/div[2]/div[1]/div/div[2]/div[1]")); 
+    	
+    	subMenuOption.click();
+    	
+    	
+    	WebElement Location = driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[3]/div[1]/div[2]/div/form/div[2]/div[2]/div"));
+    	//Mouse hover Location 
+    	actions.moveToElement(Location).perform();
+    	//Now Select 'Hall 3' from sub menu which has got displayed on mouse hover of 'Location'
+    	WebElement subMenu_loc_Option = driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[3]/div[1]/div[2]/div/form/div[2]/div[2]/div/div[2]/div[2]")); 
+    	
+    	subMenu_loc_Option.click();
+    	
+    	driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[3]/div[1]/div[2]/div/form/div[3]/div/input")).sendKeys("D 304 Washroom");
+		driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[3]/div[1]/div[2]/div/form/div[4]/input")).sendKeys("Leakage in bathroom");
+		driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[3]/div[1]/div[2]/div/form/button")).click();
 	}
 	
 	
