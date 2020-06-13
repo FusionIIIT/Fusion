e164672692c6a303e72f568bb2022726760b8c9f
Sachin <2017219@iiitdmj.ac.in>
2020-06-13 12:45:58 +0530

    Added Automatic Report Generation

--- Test/Fusion Login Auth DDT/src/main/java/com/fusion/complaintmodule/TestRunner.java
+++ Test/Fusion Login Auth DDT/src/main/java/com/fusion/complaintmodule/TestRunner.java
@@ -4,7 +4,7 @@ package com.fusion.complaintmodule;
 import cucumber.api.CucumberOptions;
 
 
-@CucumberOptions(features="/src/main/resources/main.feature",glue="com.fusion.complaintmodule")
+@CucumberOptions(features="/src/main/resources/main.feature",glue="com.fusion.complaintmodule",plugin="html:target/test-report")
 public class TestRunner{
 	
 }
