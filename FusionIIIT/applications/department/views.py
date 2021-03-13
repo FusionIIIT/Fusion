5583ab0555d29988b2b2ed3bd08df1e578d75304
Manish Kumar Sharma <2018141@iiitdmj.ac.in>
2021-03-13 22:37:58 +0530

    BTechStudents template created,student data queried and showed

--- FusionIIIT/applications/department/views.py
+++ FusionIIIT/applications/department/views.py
@@ -36,4 +36,23 @@ def hod(request):
 def file_complaint(request):
     #return render(request, "department/dep_complaint.html")
     return render(request, 'department/dep_complaint.html')
-    
+
+def BtechFirstYear_Students(request):
+    student_list1=Student.objects.filter(batch=2019)
+    id_dict={'student_list':student_list1,'batch':"First"}
+    return render(request, 'department/BTechStudents.html',context=id_dict)
+
+def BtechSecondYear_Students(request):
+    student_list2=Student.objects.filter(batch=2018)
+    id_dict={'student_list':student_list2,'batch':"Second"}
+    return render(request, 'department/BTechStudents.html',context=id_dict)
+
+def BtechThirdYear_Students(request):
+    student_list3=Student.objects.filter(batch=2017)
+    id_dict={'student_list':student_list3,'batch':"Third"}
+    return render(request, 'department/BTechStudents.html',context=id_dict)
+
+def BtechFinalYear_Students(request):
+    student_list4=Student.objects.filter(batch=2016)
+    id_dict={'student_list':student_list4,'batch':"Final"}
+    return render(request, 'department/BTechStudents.html',context=id_dict)
