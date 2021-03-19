a5eb8cfe7d624980ca18462ae3270ec1863c6e81
Pratham Kushwah <kushwahpratham123@gmail.com>
2021-03-19 09:09:36 +0530

    Change in model - added fields

--- FusionIIIT/applications/alumni/models.py
+++ FusionIIIT/applications/alumni/models.py
@@ -3,22 +3,30 @@ from django.db import models
 # Create your models here.
 
 class Job(models.Model):
-    start_date = models.DateField()
-    end_date=models.DateField()
-    skill_required=models.TextField(max_length=500)
-    job_title = models.CharField(max_length=300)
-    job_description = models.TextField()
-    job_recommendedby = models.ForeignKey(Alumni,on_delete=models.CASCADE)
-    job_company = models.TextField()
+    id                  = models.AutoField(primary_key=True)
+    start_date          = models.DateField()
+    end_date            = models.DateField()
+    skill_required      = models.TextField(max_length=500)
+    job_title           = models.CharField(max_length=300)
+    job_description     = models.TextField()
+    job_recommendedby   = models.ForeignKey(Alumni,on_delete=models.CASCADE)
+    job_company         = models.TextField()
+    job_link            = models.URLField()
+
+    def __str__(self):
+        return str(self.id)
 
     class Meta:
         db_table = 'Job'
 
 class Alumni(models.Model):
-    alumni_roll = models.IntegerField(primary_key=True, null=False)
-    alumni_name = models.TextField()
-    alumni_linkedinurl = models.URLField()
-    alumni_batch = models.IntegerField()
+    alumni_roll         = models.IntegerField(primary_key=True, null=False, blank=false)
+    alumni_name         = models.TextField()
+    alumni_linkedinurl  = models.URLField()
+    alumni_batch        = models.IntegerField()
+    alumni_experience   = models.TextField()
+    alumni_role         = models.TextField()
+    alumni_company      = models.TextField()
 
     def __str__(self):
         return str(self.alumni_roll)
