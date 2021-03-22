4d2a640782a830c719166a75db3ded1b50c43b62
tarang75490 <2018383@iiitdmj.ac.in>
2021-03-22 23:05:35 +0530

    changes after repush

---
+++ FusionIIIT/applications/counselling_cell/handlers.py
@@ -0,0 +1,30 @@
+from  .models import FAQ
+
+def add_counselling_faq(request, student):
+    """
+    This function is to record the feedback submitted
+    :param request:
+        description: Description of feedback
+        feedback_type: Type of feedback
+    :param student: Student placing the request
+    :variable:
+         extra_info: Extra information of the user
+         date_today: Today's date
+         feedback_object: Object of Feedback to store current variables
+    :return:
+        data: to record success or any errors
+    """
+    # date_today = datetime.now().date()
+    # mess_optn = Messinfo.objects.get(student_id=student)
+    answer = request.POST.get('answer')
+    category = request.POST.get('category')
+    question = request.POST.get('question')
+    print(answer)
+    # faq_object = FAQ(answer=answer,question=question,category=category)
+
+    # faq_object.save()
+    # data = {
+    #     'status': 1
+    # }
+    return data
+
---
+++ FusionIIIT/applications/counselling_cell/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'counselling_cell'
+
+urlpatterns = [
+    url(r'^$',views.counselling_cell,name="counselling"),
+    url(r'^raise_issue/',views.raise_issue,name="raiseissue"),
+    url(r'^submitfaq/',views.submit_counselling_faq,name="submitfaq")
+    
+]
