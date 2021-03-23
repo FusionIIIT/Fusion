25b831438ef3005438bc25b95bcc721d17d710a0
lanka <saikrishnalanka75@gmail.com>
2021-03-23 21:53:54 +0530

    Changed some models

--- FusionIIIT/applications/income_expenditure/models.py
+++ FusionIIIT/applications/income_expenditure/models.py
@@ -3,30 +3,30 @@ from django.utils import timezone
 
 
 class ExpenditureType(models.Model):
-	expenditure_type = models.TextField(max_length=100, blank=False)
+	expenditure_type = models.TextField(max_length=100)
 
 	def __str__(self):
         return self.expenditure_type
 
 class Expenditure(models.Model):
 	spent_on = models.ForeignKey(ExpenditureType, related_name="expenditureType", on_delete=models.PROTECT)
-	amount = models.IntegerField(null=False)
+	amount = models.IntegerField()
 	date_added = models.DateTimeField(auto_now_add=True)
-	granted_to = models.CharField(max_length=100, blank=False)
-	expenditure_receipt = models.FileField(blank=False, upload_to = 'iemodule/expenditure_receipts')
+	granted_to = models.CharField(max_length=100)
+	expenditure_receipt = models.FileField( upload_to = 'iemodule/expenditure_receipts')
 
 	def __str__(self):
         return str(self.amount)
 
 class IncomeSource(models.Model):
-	income_source = models.TextField(max_length=100, blank=False)
+	income_source = models.TextField(max_length=100)
 
 	def __str__(self):
         return self.income_source
 
 class Income(models.Model):
 	source = models.ForeignKey(IncomeSource, related_name="incomeSource", on_delete=models.PROTECT)
-	amount = models.IntegerField(null=False)
+	amount = models.IntegerField()
 	source_account = models.CharField(null=True)
 	date_added = models.DateTimeField(auto_now_add=True)
 	granted_by = models.CharField(max_length=100, blank=True)
