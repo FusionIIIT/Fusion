e484dbbbccf02a8ba727dfbb99906e5104510189
lanka <saikrishnalanka75@gmail.com>
2021-03-31 10:08:51 +0530

    Skeletal of the page is constructed

--- FusionIIIT/applications/income_expenditure/views.py
+++ FusionIIIT/applications/income_expenditure/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
 
 def main_page(request):
-	return render(request, '../templates/income_expenditure/ie.html')
+	return render(request, '../templates/incomeExpenditure/ie.html')
