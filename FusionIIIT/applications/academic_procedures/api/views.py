a29f013ebd17468381d27109453908b42b07442f
Shrey Goel <45508538+shreygoel7@users.noreply.github.com>
2021-04-07 18:27:48 +0530

    Update academic procedures api

--- FusionIIIT/applications/academic_procedures/api/views.py
+++ FusionIIIT/applications/academic_procedures/api/views.py
@@ -10,13 +10,18 @@ from rest_framework.decorators import api_view, permission_classes,authenticatio
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
+from applications.academic_information.models import Curriculum
 from applications.globals.models import HoldsDesignation, Designation, ExtraInfo
 
 from applications.academic_procedures.views import (get_user_semester, get_acad_year,
                                                     get_currently_registered_courses,
                                                     get_current_credits, get_branch_courses,
                                                     Constants, get_faculty_list,
-                                                    get_registration_courses)
+                                                    get_registration_courses, get_add_course_options,
+                                                    get_dropped_courses_count, get_added_course_count,
+                                                    get_pre_registration_eligibility,
+                                                    get_final_registration_eligibility,
+                                                    get_add_or_drop_course_date_eligibility)
 
 from . import serializers
 
@@ -67,7 +72,12 @@ def academic_procedures_faculty(request):
 @api_view(['GET'])
 def academic_procedures_student(request):
     current_user = request.user
-    current_user_data = serializers.UserSerializer(current_user).data
+    current_user_data = {
+        'first_name': current_user.first_name,
+        'last_name': current_user.last_name,
+        'username': current_user.username,
+        'email': current_user.email
+    }
     user_details = current_user.extrainfo
     des = current_user.holds_designations.all().first()
     if str(des.designation) == 'student':
@@ -122,13 +132,15 @@ def academic_procedures_student(request):
         acad_year = get_acad_year(user_sem, current_year)
         user_branch = user_details.department.name
         cpi = obj.cpi
+        cur_spi='Sem results not available' # To be fetched from db if result uploaded
 
         details = {
             'current_user': current_user_data,
             'year': acad_year,
             'user_sem': user_sem,
             'user_branch' : str(user_branch),
-            'cpi' : cpi
+            'cpi' : cpi,
+            'spi' : cur_spi
         }
         currently_registered_courses = get_currently_registered_courses(user_details.id, user_sem)
         currently_registered_courses_data = serializers.CurriculumSerializer(currently_registered_courses, many=True).data
@@ -154,10 +166,15 @@ def academic_procedures_student(request):
         fee_payment_mode_list = dict(Constants.PaymentMode)
 
         next_sem_branch_registration_courses = get_registration_courses(next_sem_branch_courses)
+        next_sem_branch_registration_courses_data = []
+        for choices in next_sem_branch_registration_courses:
+            next_sem_branch_registration_courses_data.append(serializers.CurriculumSerializer(choices, many=True).data)
         # next_sem_branch_registration_courses_data = serializers.CurriculumSerializer(next_sem_branch_registration_courses, many=True).data
 
         final_registration_choices = get_registration_courses(get_branch_courses(request.user, user_sem, user_branch))
-        # final_registration_choices_data = serializers.CurrxiculumSerializer(final_registration_choices, many=True).data
+        final_registration_choices_data = []
+        for choices in final_registration_choices:
+            final_registration_choices_data.append(serializers.CurriculumSerializer(choices, many=True).data)
 
         performance_list = []
         result_announced = False
@@ -167,22 +184,118 @@ def academic_procedures_student(request):
             except:
                 performance_obj = None
             performance_list.append(performance_obj)
+        performance_list_data = serializers.SemesterMarksSerializer(performance_list, many=True).data
+
+        thesis_request_list = serializers.ThesisTopicProcessSerializer(obj.thesistopicprocess_set.all(), many=True).data
+
+        pre_existing_thesis_flag = True if obj.thesistopicprocess_set.all() else False
+
+        current_sem_branch_courses = get_branch_courses(current_user, user_sem, user_branch)
+        add_courses_options = get_add_course_options(current_sem_branch_courses, currently_registered_courses)
+        add_courses_options_data = serializers.CurriculumSerializer(add_courses_options, many=True).data
+
+        dropped_courses_count = get_dropped_courses_count(currently_registered_courses, final_registered_courses)
+
+        added_course_count = get_added_course_count(currently_registered_courses, final_registered_courses)
+
+        pre_registration_date_flag = get_pre_registration_eligibility(current_date)
+        final_registration_date_flag = get_final_registration_eligibility(current_date)
+
+        add_or_drop_course_date_flag = get_add_or_drop_course_date_eligibility(current_date)
+
+        student_registration_check_pre = obj.studentregistrationcheck_set.all().filter(semester=user_sem+1)
+        student_registration_check_final = obj.studentregistrationcheck_set.all().filter(semester=user_sem)
+        pre_registration_flag = False
+        final_registration_flag = False
+        if(student_registration_check_pre):
+            pre_registration_flag = student_registration_check_pre.pre_registration_flag
+        if(student_registration_check_final):
+            final_registration_flag = student_registration_check_final.final_registration_flag
+
+        teaching_credit_registration_course = None
+        if phd_flag:
+            teaching_credit_registration_course = Curriculum.objects.all().filter(batch = 2016, sem =6)
+        teaching_credit_registration_course_data = serializers.CurriculumSerializer(teaching_credit_registration_course, many=True).data
+
+        if student_flag:
+            try:
+                due = obj.dues_set.get()
+                lib_d = due.library_due
+                pc_d = due.placement_cell_due
+                hos_d = due.hostel_due
+                mess_d = due.mess_due
+                acad_d = due.academic_due
+            except:
+                lib_d, pc_d, hos_d, mess_d, acad_d = 0, 0, 0, 0, 0
+
+        tot_d = lib_d + acad_d + pc_d + hos_d + mess_d
+
+        registers = obj.register_set.all()
+        course_list = []
+        for i in registers:
+            course_list.append(i.curr_id)
+        attendence = []
+        for i in course_list:
+            instructors = i.curriculum_instructor_set.all()
+            pr,ab=0,0
+            for j in list(instructors):
+
+                presents = obj.student_attendance_set.all().filter(instructor_id=j, present=True)
+                absents = obj.student_attendance_set.all().filter(instructor_id=j, present=False)
+                pr += len(presents)
+                ab += len(absents)
+            attendence.append((i,pr,pr+ab))
+        attendance_data = {}
+        for course in attendence:
+            attendance_data[course[0].course_id.course_name] = {
+                'present' : course[1],
+                'total' : course[2]
+            }
+
+        branchchange_flag = False
+        if user_sem == 2:
+            branchchange_flag=True
 
         # faculty_list = serializers.HoldsDesignationSerializer(get_faculty_list(), many=True).data
 
         resp = {
             'details': details,
             'currently_registered': currently_registered_courses_data,
-            'currently_registered': currently_registered_courses,
             'pre_registered_courses' : pre_registered_courses_data,
             'pre_registered_courses_show' : pre_registered_courses_show_data,
             'final_registered_courses' : final_registered_courses_data,
             'current_credits' : current_credits,
             'courses_list': next_sem_branch_courses_data,
             'fee_payment_mode_list' : fee_payment_mode_list,
-            'next_sem_branch_registration_courses' : str(next_sem_branch_registration_courses),
-            'final_registration_choices' : str(final_registration_choices),
-            'performance_list' : performance_list
+            'next_sem_branch_registration_courses' : next_sem_branch_registration_courses_data,
+            'final_registration_choices' : final_registration_choices_data,
+            'performance_list' : performance_list_data,
+            'thesis_request_list' : thesis_request_list,
+            'student_flag' : student_flag,
+            'ug_flag' : ug_flag,
+            'masters_flag' : masters_flag,
+            'phd_flag' : phd_flag,
+            'fac_flag' : fac_flag,
+            'des_flag' : des_flag,
+            'thesis_flag' : pre_existing_thesis_flag,
+            'add_courses_options': add_courses_options_data,
+            'drop_courses_options' : currently_registered_courses_data,
+            'dropped_courses_count' : dropped_courses_count,
+            'added_course_count' : added_course_count,
+            'pre_registration_date_flag': pre_registration_date_flag,
+            'final_registration_date_flag': final_registration_date_flag,
+            'add_or_drop_course_date_flag': add_or_drop_course_date_flag,
+            'pre_registration_flag' : pre_registration_flag,
+            'final_registration_flag': final_registration_flag,
+            'teaching_credit_registration_course' : teaching_credit_registration_course_data,
+            'lib_d':lib_d,
+            'acad_d':acad_d,
+            'mess_d':mess_d,
+            'pc_d':pc_d,
+            'hos_d':hos_d,
+            'tot_d':tot_d,
+            'attendance': attendance_data,
+            'Branch_Change_Flag':branchchange_flag
             # 'faculty_list' : faculty_list
         }
         return Response(data=resp, status=status.HTTP_200_OK)
