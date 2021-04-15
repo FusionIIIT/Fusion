61fdd1ec3fab27edcb2d6274805d565b6658dfca
Subhash Reddy <2018276@iiitdmj.ac.in>
2021-04-15 18:51:36 +0530

    added backend for income

--- FusionIIIT/applications/income_expenditure/urls.py
+++ FusionIIIT/applications/income_expenditure/urls.py
@@ -5,6 +5,7 @@ appname = 'procedures'
 urlpatterns = [
     # url(r'^$', views.academic_procedures_redirect, name='redirect'),
     # url(r'^main/', views.academic_procedures, name='procedures'),
-    # url(r'^register/', views.register, name='register'),
-    url(r'^main/', views.main_page, name='main_page'),
+    
+    url(r'^main/', views.main_page, name='main-page'),
+    url(r'^addIncome/', views.add_income, name='add-income'),
 ]
--- FusionIIIT/applications/income_expenditure/views.py
+++ FusionIIIT/applications/income_expenditure/views.py
@@ -1,9 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
+from .models import (ExpenditureType, Expenditure, IncomeSource, Income)
 
 # Create your views here.
 
 def main_page(request):
-	return render(request, '../templates/incomeExpenditure/ie.html')
+	income_history = Income.objects.all()
+	income_sources = IncomeSource.objects.all()
+	return render(
+				request,
+				'../templates/incomeExpenditure/ie.html',
+				{
+					'income_sources':income_sources,
+					'income_history':income_history,
+				})
 
+	
+def add_income(request):
+	if(request.method == 'POST'):
+		source = request.POST.get('income_source')
+		source = IncomeSource.objects.filter(id=source).first()
 
+		amount = request.POST.get('amount')
+		date = request.POST.get('date_recieved')
+		receipt = request.POST.get('income_receipt')
+		granted_by = request.POST.get('granted_by')
+
+		new_i = Income(
+						source = source,
+						amount = amount,
+						date_added = date,
+						receipt = receipt,
+						granted_by = granted_by
+						)
+		new_i.save()
+	else:
+	return redirect('main-page')
