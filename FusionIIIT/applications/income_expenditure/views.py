b7dcfa30fca7b858f4f3d7fb101ac9ff5382c206
lanka <saikrishnalanka75@gmail.com>
2021-04-20 12:46:22 +0530

    Added delete option

--- FusionIIIT/applications/income_expenditure/views.py
+++ FusionIIIT/applications/income_expenditure/views.py
@@ -8,6 +8,7 @@ def main_page(request):
 	income_history = Income.objects.all()
 	income_sources = IncomeSource.objects.all()
 	expenditure_types = ExpenditureType.objects.all()
+	expenditure_history = Expenditure.objects.all()
 	return render(
 				request,
 				'../templates/incomeExpenditure/ie.html',
@@ -15,6 +16,7 @@ def main_page(request):
 					'income_sources':income_sources,
 					'income_history':income_history,
 					'expenditure_types':expenditure_types,
+					'expenditure_history':expenditure_history,
 				})
 
 
@@ -58,21 +60,37 @@ def add_expenditure_type(request):
 	return redirect('main-page')
 
 def add_expenditure(request):
-	# if(request.method == 'POST'):
-		# source = request.POST.get('income_source')
-		# source = IncomeSource.objects.filter(id=source).first()
+	if(request.method == 'POST'):
+		spent_on = request.POST.get('spent_on')
+		spent_on = ExpenditureType.objects.filter(id=spent_on).first()
 
-		# amount = request.POST.get('amount')
-		# date = request.POST.get('date_recieved')
-		# receipt = request.POST.get('income_receipt')
-		# granted_by = request.POST.get('granted_by')
+		amount = request.POST.get('amount')
+		date = request.POST.get('date_spent')
+		receipt = request.POST.get('expenditure_receipt')
+		granted_to = request.POST.get('granted_to')
 
-		# new_i = Income(
-		# 				source = source,
-		# 				amount = amount,
-		# 				date_added = date,
-		# 				receipt = receipt,
-		# 				granted_by = granted_by
-		# 				)
-		# new_i.save()
+		new_e = Expenditure(
+		 				spent_on = spent_on,
+		 				amount = amount,
+		 				date_added = date,
+						granted_to = granted_to,
+		 				expenditure_receipt = receipt,
+		 				
+		 				)
+		new_e.save()
+	return redirect('main-page')
+
+
+def del_expenditure(request):
+	if(request.method == 'POST'):
+		ex_id = request.POST.get('id')
+		Expenditure.objects.get(id=ex_id).delete()
+
+	return redirect('main-page')
+
+def del_income(request):
+	if(request.method == 'POST'):
+		in_id = request.POST.get('id')
+		Income.objects.get(id=in_id).delete()
+
 	return redirect('main-page')
