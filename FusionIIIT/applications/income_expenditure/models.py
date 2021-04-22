52611250ea4ae9156b33d436ca0ca59a369ef725
Sai Krishna Lanka <saikrishnalanka75@gmail.com>
2021-04-22 23:40:30 +0530

    Group-4 Income and Expenditure Module (#394)
    * checking
    
    * removed comment
    
    * added home page
    
    * revised errors
    
    * testing
    
    * Fixed an issue in the models
    
    * Skeletal of the page is constructed
    
    * Modified Models
    
    * Added templates
    
    * Added some forms to the module a bug is present in add expenditure type
    
    * Fixed a bug
    
    * modified1
    
    * Added migrations
    
    * error
    
    * resolved bugs
    
    * fixed bugs
    
    * Changed history page
    
    * added backend for income
    
    * backend
    
    * small change
    
    * added comment
    
    * Fixed a bug in views
    
    * added extra views
    
    * added new views
    
    * test1
    
    * msg1
    
    * Added delete option
    
    * view_stats
    
    * Created balance Sheet
    
    * test
    
    * mat
    
    * fixed_bugs
    
    * Added balance sheet
    
    * Added balance sheet
    
    * Added Fixed Attributes
    
    * Populated first part of BalanceSheet
    
    * Added date constraint
    
    * Modified models and Dropdowns
    
    * Added some features
    
    * done stats
    
    * Completed BalanceSheet
    
    * Completed Doc Strings
    
    * test
    
    * Completed
    
    * Completed
    
    * Completed
    
    * Completed
    
    * Fixed a bug
    
    * Fixed bugs
    
    Co-authored-by: sai deepchand <deepchandkunisetty.9@gmail.com>
    Co-authored-by: Subhash Reddy <2018276@iiitdmj.ac.in>

--- FusionIIIT/applications/income_expenditure/models.py
+++ FusionIIIT/applications/income_expenditure/models.py
@@ -1,36 +1,51 @@
 from django.db import models
 from django.utils import timezone
+from datetime import datetime
 
 
 class ExpenditureType(models.Model):
 	expenditure_type = models.CharField(max_length=100)
 
 	def __str__(self):
-        return self.expenditure_type
+		return self.expenditure_type
 
 class Expenditure(models.Model):
 	spent_on = models.ForeignKey(ExpenditureType, related_name="expenditureType", on_delete=models.PROTECT)
 	amount = models.IntegerField()
-	date_added = models.DateTimeField(auto_now_add=True)
-	granted_to = models.CharField(max_length=100)
+	date_added = models.DateField()
+	remarks = models.CharField(max_length=100)
 	expenditure_receipt = models.FileField( upload_to = 'iemodule/expenditure_receipts')
 
 	def __str__(self):
-        return str(self.amount)
+		return str(self.amount)
 
 class IncomeSource(models.Model):
 	income_source = models.CharField(max_length=100)
 
 	def __str__(self):
-        return self.income_source
+		return self.income_source
 
 class Income(models.Model):
 	source = models.ForeignKey(IncomeSource, related_name="incomeSource", on_delete=models.PROTECT)
 	amount = models.IntegerField()
-	source_account = models.CharField(max_length = 100, null=True)
-	date_added = models.DateTimeField(auto_now_add=True)
-	granted_by = models.CharField(max_length=100, blank=True)
+	date_added = models.DateField()
+	remarks = models.CharField(max_length=100, blank=True)
 	receipt = models.FileField(blank=True, upload_to = 'iemodule/income_receipts')
 
 	def __str__(self):
-        return str(self.amount)
+		return str(self.amount)
+
+class FixedAttributes(models.Model):
+	attribute = models.CharField(max_length=100)
+	value = models.IntegerField(default=0)
+
+	def __str__(self):
+		return str(self.value)
+
+class BalanceSheet(models.Model):
+	balanceSheet = models.FileField()
+	date_added = models.CharField(max_length=10)
+
+	def __str__(self):
+		return str(self.date_added)
+
