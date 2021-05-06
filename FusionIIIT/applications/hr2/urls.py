d8871a55af692c521bbefcebe94b5faea29d7fad
Pranav Choudhary <2018184@iiitdmj.ac.in>
2021-05-06 23:46:57 +0530

    Hradmin changes

--- FusionIIIT/applications/hr2/urls.py
+++ FusionIIIT/applications/hr2/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     url(r'^edit/(?P<id>\d+)/$', views.edit_employee_details, name='editEmployeeDetails'),
     url(r'^viewdetails/(?P<id>\d+)/$', views.view_employee_details, name='viewEmployeeDetails'),
     url(r'^editServiceBook/(?P<id>\d+)/$', views.edit_employee_servicebook, name='editServiceBook'),
+    url(r'^administrativeProfile/$', views.administrative_profile, name='administrativeProfile'),
     
 ]
 
