185e1d22606d928fa509a3e0422c9cae18627d80
Your Name <you@example.com>
2021-05-07 20:46:43 +0530

    add status color for created indent

--- FusionIIIT/Fusion/settings/development.py
+++ FusionIIIT/Fusion/settings/development.py
@@ -10,9 +10,9 @@ DATABASES = {
      'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'fusionlab',
-        'HOST': 'localhost',
-        'USER': 'postgres',
-        'PASSWORD': 'Postgre@123',
+        'HOST': '172.27.16.216',
+        'USER': 'fusion_admin',
+        'PASSWORD': 'hello123',
     }
 }
 
