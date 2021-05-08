4d08dd309c1baaf559bb395ec0745b9b37e9cb08
nikita290599 <2018168@iiitdmj.ac.in>
2021-05-08 12:52:15 +0530

    group8 and 9 : Added feature of notifications

--- FusionIIIT/notification/views.py
+++ FusionIIIT/notification/views.py
@@ -328,3 +328,20 @@ def AssistantshipClaim_account_notify(sender,stu, recipient):
     url = 'academic-procedures:academic_procedures'
     module = 'Assistantship Request'
     notify.send(sender=sender,recipient= recipient, url=url, module=module, verb=message)
+
+def office_module_DeanRSPC_notif(sender, recipient, type):
+    url='office:officeOfDeanRSPC'
+    module='Office Module'
+    sender = sender
+    recipient = recipient
+    verb = ""
+
+    if type == 'Approve':
+        verb = "Your Project request has been accepted"
+    elif type == 'Disapprove':
+        verb = "Your project request got rejected ."
+    elif type == 'Pending':
+        verb = "Kindly wait for the response"
+
+
+    notify.send(sender=sender, recipient=recipient, url=url, module=module, verb=verb)
