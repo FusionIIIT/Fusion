8b0a164b1e92d865a66d0d01666693f17ae4b763
Your Name <you@example.com>
2021-05-12 14:47:52 +0530

    added stock entry urls

--- FusionIIIT/Fusion/settings/development.py
+++ FusionIIIT/Fusion/settings/development.py
@@ -10,9 +10,9 @@ DATABASES = {
      'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'fusionlab',
-        'HOST': '172.27.16.216',
-        'USER': 'fusion_admin',
-        'PASSWORD': 'hello123',
+        'HOST': 'localhost',
+        'USER': 'postgres',
+        'PASSWORD': 'Postgre@123',
     }
 }
 
