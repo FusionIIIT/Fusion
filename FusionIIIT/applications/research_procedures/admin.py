f74b789c8b4ae6979c6b36cb43b87dd5d1dc7624
Nagaraju <nagarajrepala@gmail.com>
2022-01-26 19:21:19 +0530

    Patents are now visible for students

--- FusionIIIT/applications/research_procedures/admin.py
+++ FusionIIIT/applications/research_procedures/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 from applications.research_procedures.models import Patent
+from django.utils.html import format_html
+
+# Adding a custom admin view for patent
+class PatentAdmin(admin.ModelAdmin):
+
+    def _status(self, obj):
+        color = "orange"
+        if obj.status == "Approved":
+            color = "green"
+        elif obj.status == "Disapproved":
+            color = "red"
+        return format_html('<span style="color: %s"><b>%s</b></span>' % (color, obj.status))
+
+    list_display = ["faculty_id.user","title","_status"]
 
 # Register your models here.
-admin.site.register(Patent)
+admin.site.register(Patent,PatentAdmin)
