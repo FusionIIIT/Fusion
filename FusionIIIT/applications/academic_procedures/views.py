66cdb567a8c180bdebad1672df8805281611f17a
animesh <akanimesh@MSI.localdomain>
2022-02-19 12:10:28 +0530

    added batch in courseInstructor table

--- FusionIIIT/applications/academic_procedures/views.py
+++ FusionIIIT/applications/academic_procedures/views.py
@@ -152,9 +152,7 @@ def academic_procedures_faculty(request):
         approved_assistantship_request_list = ta_approved_assistantship_request_list | thesis_approved_assistantship_request_list
         mtechseminar_request_list = MTechGraduateSeminarReport.objects.all().filter(Overall_grade = '')
         phdprogress_request_list = PhDProgressExamination.objects.all().filter(Overall_grade = '')
-        #courses_list = Curriculum_Instructor.objects.select_related('curriculum_id','instructor_id','curriculum_id__course_id','instructor_id__department','instructor_id__user').filter(instructor_id=user_details).filter(curriculum_id__sem__in = sem)
-        courses_list = list(CourseInstructor.objects.select_related('course_id','instructor_id').filter(instructor_id=user_details))
-        
+        courses_list = list(CourseInstructor.objects.select_related('course_id', 'batch_id', 'batch_id__discipline').filter(instructor_id__id=5021).only('course_id__code', 'course_id__name', 'batch_id'))
         r = range(4)
         return render(
                         request,
