3dccec3b430d2bd64d5e92aa23085b3a6afc2f57
frazaatif <imaatiffraz@gmail.com>
2022-03-13 18:21:06 +0530

    Solves FusionIIIT#782

--- FusionIIIT/applications/hostel_management/views.py
+++ FusionIIIT/applications/hostel_management/views.py
@@ -20,7 +20,8 @@ from django.template.loader import get_template
 from django.views.generic import View
 from django.db.models import Q
 from django.contrib import messages
-from .utils import render_to_pdf,update_hostel_room,save_worker_report_sheet,get_caretaker_hall
+from .utils import render_to_pdf, save_worker_report_sheet,get_caretaker_hall
+from .utils import add_to_room, remove_from_room
 
 @login_required
 def hostel_view(request, context={}):
@@ -75,7 +76,7 @@ def hostel_view(request, context={}):
         get_hall_num=re.findall('[0-9]+',str(get_hall.hall_id))
         hall_student=Student.objects.filter(hall_no=int(str(get_hall_num[0]))).select_related('id__user')
         current_hall='hall'+str(get_hall_num[0])
-
+    
 
     hall_caretaker_user=[]
     for caretaker in hall_caretakers:
@@ -237,11 +238,10 @@ def edit_student_room(request):
     if request.method == "POST":
         roll_no = request.POST["roll_no"]
         room_no = request.POST["room_no"]
-        batch=str(roll_no[0])+str(roll_no[1])+str(roll_no[2])+str(roll_no[3])
-        students = Student.objects.filter(batch=int(batch)).select_related('id__user')
-        for student in students:
-            if str(student.id.user)==str(roll_no):
-                update_hostel_room(student,room_no)
+        
+        student = Student.objects.get(id=roll_no)
+        remove_from_room(student)
+        add_to_room(student, new_room=room_no)
         messages.success(request, 'Student room changed successfully.')
         return HttpResponseRedirect(reverse("hostelmanagement:hostel_view"))
 
