30bc8e97c3d4e1c4a78389b216889e8421458309
Aatif Fraz <imaatiffraz@gmail.com>
2022-03-30 15:01:43 +0530

    Solved FusionIIIT#802 ,Implemented attendance

--- FusionIIIT/applications/hostel_management/views.py
+++ FusionIIIT/applications/hostel_management/views.py
@@ -91,6 +91,12 @@ def hostel_view(request, context={}):
     else:
         worker_report = WorkerReport.objects.filter(hall__hall_id=current_hall, year=current_year-1, month=12)
 
+    attendance = HostelStudentAttendence.objects.all().select_related()
+    halls_attendance = {}
+    for hall in all_hall:
+        halls_attendance[hall.hall_id] = HostelStudentAttendence.objects.filter(hall=hall).select_related()
+
+
     context = {
         
         'all_hall': all_hall,
@@ -107,6 +113,7 @@ def hostel_view(request, context={}):
         'current_hall' : current_hall,
         'hall_staffs': hall_staffs,
         'hall_notices': hall_notices,
+        'attendance': halls_attendance,
         **context
     }
 
@@ -244,6 +251,34 @@ def edit_student_room(request):
         add_to_room(student, new_room=room_no)
         messages.success(request, 'Student room changed successfully.')
         return HttpResponseRedirect(reverse("hostelmanagement:hostel_view"))
+
+def edit_attendance(request):
+    """
+    This function is used to edit the attendance of a student.
+    @param:
+      request - HttpRequest object containing metadata about the user request.
+    
+    @variables:
+      student_id = The student whose attendance has to be updated.
+      hall = The hall of the concerned student.
+      date = The date on which attendance has to be marked.
+    """
+    if request.method == "POST":
+        roll_no = request.POST["roll_no"]
+        
+        student = Student.objects.get(id=roll_no)
+        hall = Hall.objects.get(hall_id='hall'+str(student.hall_no))
+        date = datetime.datetime.today().strftime('%Y-%m-%d')
+
+        record = HostelStudentAttendence.objects.create(student_id=student, \
+            hall=hall, date=date, present=True)
+        record.save()
+
+        messages.success(request, f'Attendance of {student.id.id} recorded.')
+
+        return HttpResponseRedirect(reverse("hostelmanagement:hostel_view"))
+
+
 
 
 @login_required
