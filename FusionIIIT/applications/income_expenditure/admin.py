99951eedb64ecc1c447df9172dece0f0b633afa4
Chetan Pawar <91899607+chetan1357@users.noreply.github.com>
2022-04-02 11:08:10 +0530

    Update admin.py

--- FusionIIIT/applications/income_expenditure/admin.py
+++ FusionIIIT/applications/income_expenditure/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import (ExpenditureType, Expenditure, IncomeSource, Income, FixedAttributes, BalanceSheet)
+
+
+from .models import (ExpenditureType, Expenditure, IncomeSource, Income, FixedAttributes, BalanceSheet,otherExpense)
 
 admin.site.register(ExpenditureType)
 admin.site.register(Expenditure)
@@ -8,3 +10,4 @@ admin.site.register(IncomeSource)
 admin.site.register(Income)
 admin.site.register(FixedAttributes)
 admin.site.register(BalanceSheet)
+admin.site.register(otherExpense)
