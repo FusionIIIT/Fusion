9848005adecd98b6f92979628f4e81c265339053
kunaltulsidasani <tulsidasanikunal@gmail.com>
2022-04-28 19:38:11 +0530

    Added models in django admin

--- FusionIIIT/applications/department/admin.py
+++ FusionIIIT/applications/department/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-"""from .models import ()
+from .models import(Announcements, SpecialRequest)
 
-admin.site.register(),"""
+admin.site.register(Announcements)
+admin.site.register(SpecialRequest)
