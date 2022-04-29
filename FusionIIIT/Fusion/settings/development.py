799a8c5532344b46975e3592c1339b23051a7a09
Ujjawal2111 <60523537+Ujjawal2111@users.noreply.github.com>
2022-04-29 17:11:28 +0530

    f

--- FusionIIIT/Fusion/settings/development.py
+++ FusionIIIT/Fusion/settings/development.py
@@ -9,8 +9,8 @@ ALLOWED_HOSTS = []
 DATABASES = {
      'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'fusionlab',
-        'HOST': '172.27.16.216',
+        'NAME': 'fusionlab3',
+        'HOST': '172.27.16.215',
         'USER': 'fusion_admin',
         'PASSWORD': 'hello123',
     }
