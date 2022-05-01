6a35d4cb3bb26625e04171d48da832587bd580be
Prashant <prashantkrsaroj@gmail.com>
2022-05-02 00:17:59 +0530

    updated changes

--- FusionIIIT/Fusion/settings/development.py
+++ FusionIIIT/Fusion/settings/development.py
@@ -10,7 +10,7 @@ DATABASES = {
      'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'fusionlab',
-        'HOST': '172.27.16.216',
+        'HOST': 'localhost',
         'USER': 'fusion_admin',
         'PASSWORD': 'hello123',
     }
