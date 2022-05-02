845652573d6e56cc2dbe3b6baf5c49e00e48e6f1
Nagaraju <nagarajrepala@gmail.com>
2022-05-02 19:25:54 +0530

    Added Projects in Research Procedures

--- FusionIIIT/applications/research_procedures/urls.py
+++ FusionIIIT/applications/research_procedures/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
       url(r'^$', views.patent_registration, name='patent_registration'),
       url(r'^update$', views.patent_status_update, name='patent_status_update'),
       url(r'^research_group$', views.research_group_create, name='research_group_create'),
+      url(r'^project_insert$',views.project_insert,name='project_insert'),
+      url(r'^consult_insert$',views.consult_insert,name='consult_insert'),
+      url(r'^transfer_insert$',views.transfer_insert,name='transfer_insert'),
       url(r'^api/',include('applications.research_procedures.api.urls')),
+
 ]
