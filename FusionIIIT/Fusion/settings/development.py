9700c0d4709dacddc2c8b866d291eb78acf97294
Abdul Hasnain Khan <ahkhan647@gmail.com>
2022-05-19 12:14:22 +0530

    corrected rating precision in feedback page

--- FusionIIIT/Fusion/settings/development.py
+++ FusionIIIT/Fusion/settings/development.py
@@ -10,9 +10,9 @@ DATABASES = {
      'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'fusionlab',
-        'HOST': 'localhost',
-        'USER': 'fusion_admin',
-        'PASSWORD': 'hello123',
+        'HOST': '127.0.0.1',
+        'USER': 'abdulhasnainkhan',
+        'PASSWORD': '12345',
     }
 }
 
