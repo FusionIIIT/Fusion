432d9772a56a9162c5e33682aefb6bd9b8f47e4a
Lawful2002 <sharshvardhan101@gmail.com>
2023-03-16 16:58:26 +0530

    merging

--- FusionIIIT/Fusion/settings/development.py
+++ FusionIIIT/Fusion/settings/development.py
@@ -10,9 +10,9 @@ DATABASES = {
      'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'fusionlab',
-        'HOST': os.environ.get("DB_HOST", default='localhost'),
-        'USER': 'fusion_admin',
-        'PASSWORD': 'hello123',
+        'HOST': 'localhost',
+        'USER': 'fusionuser',
+        'PASSWORD': 'password',
     }
 }
 
