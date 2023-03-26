90d983ae851f0139bea0869824fd023c3a0ee33e
Kanha Tiwari <tiwarikanha26@gmail.com>
2023-03-26 16:09:23 +0530

    conflict resolved in urls.py

--- FusionIIIT/applications/ps1/urls.py
+++ FusionIIIT/applications/ps1/urls.py
@@ -28,5 +28,4 @@ urlpatterns = [
     url(r'^create_indent_multiple/$',views.create_indent_multiple, name='create_indent_multiple'),
     url(r'^drafts1/$', views.drafts_for_multiple_item, name='drafts_for_multiple_item'),
     url(r'^draftview1/(?P<id>\d+)$', views.draftview_multiple_items_indent, name='draftview_multiple_items_indent')
-
 ]
