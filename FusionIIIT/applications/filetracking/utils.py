7af7ee0d51ed31278ec219444aa12d9907a64707
abhishek12071 <92369178+abhishek12071@users.noreply.github.com>
2023-03-27 16:56:55 +0530

    Update utils.py

--- FusionIIIT/applications/filetracking/utils.py
+++ FusionIIIT/applications/filetracking/utils.py
@@ -5,3 +5,7 @@ from django.contrib.auth.models import User
 def get_designation(userid):
     user_designation=HoldsDesignation.objects.select_related('user','working','designation').filter(user=userid)
     return user_designation
+
+def get_all_designation():
+     user_designation=HoldsDesignation.objects.select_related('user','working','designation').all()
+     return user_designation
