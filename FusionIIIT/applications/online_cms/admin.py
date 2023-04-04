3fba8563adae1392d74839449584e24c269c83d7
tech-akash <76513524+tech-akash@users.noreply.github.com>
2023-04-04 14:18:41 +0530

    ac-3: week-8: Prof can submit Marks to assignment and made site responsive (#1156)
    Co-authored-by: “tech-akash” <“20bcs020@iiitdmj.ac.in”>
    Co-authored-by: Akash Singh <akash@pop-os.localdomain>

--- FusionIIIT/applications/online_cms/admin.py
+++ FusionIIIT/applications/online_cms/admin.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 
 from .models import (Assignment, CourseDocuments, CourseVideo, Forum,
                      ForumReply, Quiz, QuizQuestion, QuizResult, StudentAnswer,
-                     StudentAssignment, Topics,CourseSlide)
+                     StudentAssignment1, Topics,CourseAssignment,CourseSlide)
 
 class QuizResultAdmin(admin.ModelAdmin):
     model = QuizResult
     raw_id_fields = ("student_id",)
 
 admin.site.register(CourseDocuments)
+admin.site.register(CourseAssignment)
 admin.site.register(CourseSlide)
 admin.site.register(CourseVideo)
 
@@ -22,7 +23,7 @@ admin.site.register(StudentAnswer)
 
 admin.site.register(Assignment)
 
-admin.site.register(StudentAssignment)
+admin.site.register(StudentAssignment1)
 
 admin.site.register(QuizResult, QuizResultAdmin)
 
