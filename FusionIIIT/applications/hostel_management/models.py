c955e5075cf1c0541bd0b1a33e2be4c18a8c440e
Harsh Kumar Singh <harshkumarsinghpawar@gmail.com>
2023-04-06 10:24:55 +0530

    some random files

--- FusionIIIT/applications/hostel_management/models.py
+++ FusionIIIT/applications/hostel_management/models.py
@@ -33,7 +33,14 @@ class HostelManagementConstants:
     ("CheckedIn" , 'Checked In'),
     ("Complete", 'Complete'),
     ("Forward", 'Forward')
-    )    
+    )
+
+    HALL_NAMES = (
+        ('saraswati', 'saraswati'),
+        ('vivekananda', 'vivekananda'),
+        ('aryabhatta', 'aryabhatta'),
+        ('vashishtha', 'vashishtha'),
+    )   
 
 
 class Hall(models.Model):
@@ -44,8 +51,8 @@ class Hall(models.Model):
     'max_accomodation' stores maximum accomodation limit of a Hall of Residence.
     'number_students' stores number of students currently residing in a Hall of Residence.
     """
-    hall_id = models.CharField(max_length=10)
-    hall_name = models.CharField(max_length=50)
+    hall_id = models.CharField(max_length=255)
+    hall_name = models.CharField(max_length=255, choices=HostelManagementConstants.HALL_NAMES)
     max_accomodation = models.IntegerField(default=0)
     number_students = models.PositiveIntegerField(default=0)
 
@@ -80,22 +87,18 @@ class HallWarden(models.Model):
     def __str__(self):
         return str(self.hall) + '  (' + str(self.faculty.id.user.username) + ')'
     
-
-class GuestRoomDetail(models.Model):
+class HallAdmin(models.Model):
     """
-    Records information related to guest rooms in Hall of Residences.
+    Records the admin of Hall of Residences.
 
     'hall' refers to the related Hall of Residence.
-    'room_no' stores the guest room number.
-    'room_status' stores the current status of the guest room from the available choices in 'ROOM_STATUS'.
+    'faculty' refers to the acting admin for the same Hall of Residence.
     """
     hall = models.ForeignKey(Hall, on_delete=models.CASCADE)
-    room_no = models.CharField(max_length=4, unique=True)
-    room_status  = models.CharField(max_length=20, choices=HostelManagementConstants.ROOM_STATUS, default='Available')
+    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.room_no
-
+        return str(self.hall) + 'admin ' + '(' + str(self.faculty) + ')'
 
 class GuestRoomBooking(models.Model):
     """
@@ -115,22 +118,22 @@ class GuestRoomBooking(models.Model):
     'nationality' stores the nationality of the guests.
     """    
     hall = models.ForeignKey(Hall, on_delete=models.CASCADE)
-    intender = models.ForeignKey(User, on_delete=models.CASCADE)
-    guest_name = models.CharField(max_length=100)
-    guest_phone = models.CharField(max_length=15)
-    guest_email = models.CharField(max_length=40, blank=True)
+    intender = models.ForeignKey(ExtraInfo, on_delete=models.CASCADE)
+    guest_name = models.CharField(max_length=255)
+    guest_phone = models.CharField(max_length=255)
+    guest_email = models.CharField(max_length=255, blank=True)
     guest_address = models.TextField(blank=True)
-    rooms_required =  models.IntegerField(default=1,null=True,blank=True)
-    guest_room_id = models.ManyToManyField(GuestRoomDetail)
+    rooms_required =  models.IntegerField(default=1, null=True, blank=True)
+    guest_room_id = models.CharField(max_length=255, blank=True)
     total_guest = models.IntegerField(default=1)
     purpose = models.TextField()
     arrival_date = models.DateField(auto_now_add=False, auto_now=False)
     arrival_time = models.TimeField(auto_now_add=False, auto_now=False)
     departure_date = models.DateField(auto_now_add=False, auto_now=False)
     departure_time = models.TimeField(auto_now_add=False, auto_now=False)
-    status = models.CharField(max_length=15, choices=HostelManagementConstants.BOOKING_STATUS ,default ="Pending")
+    status = models.CharField(max_length=255, choices=HostelManagementConstants.BOOKING_STATUS ,default ="Pending")
     booking_date = models.DateField(auto_now_add=False, auto_now=False, default=timezone.now)
-    nationality = models.CharField(max_length=20, blank=True)
+    nationality = models.CharField(max_length=255, blank=True)
     
     def __str__(self):
         return '%s ----> %s - %s' % (self.id, self.guest_name, self.status)
@@ -148,8 +151,8 @@ class StaffSchedule(models.Model):
     """    
     hall = models.ForeignKey(Hall, on_delete=models.CASCADE)
     staff_id = models.ForeignKey(Staff, on_delete=models.ForeignKey)
-    staff_type = models.CharField(max_length=100, default='Caretaker')
-    day = models.CharField(max_length=15, choices=HostelManagementConstants.DAYS_OF_WEEK)
+    staff_type = models.CharField(max_length=255, default='Caretaker')
+    day = models.CharField(max_length=255, choices=HostelManagementConstants.DAYS_OF_WEEK)
     start_time = models.TimeField(null=True,blank=True)
     end_time = models.TimeField(null=True,blank=True)
 
@@ -169,7 +172,7 @@ class HostelNoticeBoard(models.Model):
     """    
     hall = models.ForeignKey(Hall, on_delete=models.CASCADE)
     posted_by = models.ForeignKey(ExtraInfo, on_delete=models.ForeignKey)
-    head_line = models.CharField(max_length=100)
+    head_line = models.CharField(max_length=255)
     content = models.FileField(upload_to='hostel_management/', blank=True, null=True)
     description = models.TextField(blank=True)
 
@@ -205,8 +208,8 @@ class HallRoom(models.Model):
     'room_occupied' stores the current number of occupants of a room.
     """    
     hall = models.ForeignKey(Hall, on_delete=models.CASCADE)
-    room_no = models.CharField(max_length=4) 
-    block_no = models.CharField(max_length=1)
+    room_no = models.CharField(max_length=255) 
+    block_no = models.CharField(max_length=255)
     room_cap = models.IntegerField(default=3)
     room_occupied = models.IntegerField(default=0)
 
@@ -226,14 +229,14 @@ class WorkerReport(models.Model):
     'total_day' stores the number of days in a month.
     'remark' stores remarks for a worker.
     """
-    worker_id = models.CharField(max_length=10)
+    worker_id = models.CharField(max_length=255)
     hall = models.ForeignKey(Hall, on_delete=models.CASCADE)
-    worker_name = models.CharField(max_length=50)
+    worker_name = models.CharField(max_length=255)
     year =  models.IntegerField(default=2020)
     month = models.IntegerField(default=1)
     absent = models.IntegerField(default= 0)
     total_day = models.IntegerField(default=31)
-    remark = models.CharField(max_length=100)
+    remark = models.CharField(max_length=255)
 
     def str(self):
         return str(self.worker_name)+'->' + str(self.month) + '-' + str(self.absent)   
--- FusionIIIT/applications/ps2/admin.py
+++ FusionIIIT/applications/ps2/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import StockAdmin, StockEntry
 # Register your models here.
+admin.site.register(StockAdmin)
+admin.site.register(StockEntry)
