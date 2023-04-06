735262e8d85b8cea8ab5843b3aacbc3e4a665834
Harsh Kumar Singh <harshkumarsinghpawar@gmail.com>
2023-04-06 16:21:44 +0530

    Revert "Bill generation completed"
    This reverts commit 3d86fdaf47bd4d6dcdc81f375e4b339d6de4b15c.

--- FusionIIIT/applications/visitor_hostel/migrations/0002_auto_20230401_1704.py
+++ FusionIIIT/applications/visitor_hostel/migrations/0002_auto_20230401_1704.py
@@ -15,7 +15,10 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='bill',
             name='bill_date',
-            field=models.DateField(blank=True, default=django.utils.timezone.now),
+
+
+            field=models.DateField(blank=True, default=django.utils.timezone.now, blank = True),
+
         ),
         migrations.DeleteModel(
             name='MealRecord',
