4108d329da68d8dea6c26ff78516268331902c65
Abhinandan Gaur <abhiabhigaur524@gmail.com>
2023-04-09 01:42:18 +0530

    update scholarship module files to incorporate new changes

--- FusionIIIT/applications/scholarships/admin.py
+++ FusionIIIT/applications/scholarships/admin.py
@@ -2,14 +2,15 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import (Award_and_scholarship, Director_gold, Director_silver,
-                     Mcm, Notional_prize, Previous_winner, Proficiency_dm,
+                     Mcm, Notional_prize, Previous_winner, DM_Proficiency_gold, IIITDM_Proficiency
                      Release,Notification,Application)
 
 admin.site.register(Mcm),
 admin.site.register(Award_and_scholarship),
 admin.site.register(Previous_winner),
 admin.site.register(Release),
-admin.site.register(Proficiency_dm),
+admin.site.register(DM_Proficiency_gold),
+admin.site.register(IIITDM_Proficiency),
 admin.site.register(Director_silver),
 admin.site.register(Director_gold),
 admin.site.register(Notional_prize),
