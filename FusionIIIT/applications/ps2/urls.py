541d53c510b50cd53a44161936bbb31e6bf81648
Pulkit Bhargava <pulkitbhargava222@gmail.com>
2023-04-09 21:46:03 +0530

    transfer stock functionality added

--- FusionIIIT/applications/ps2/urls.py
+++ FusionIIIT/applications/ps2/urls.py
@@ -6,5 +6,7 @@ app_name = 'ps2'
 
 urlpatterns = [
     url(r'^$', views.ps2, name='ps2'),
-    url(r'^addstock/$', views.addstock, name='addstock')
+    url(r'^addstock/$', views.addstock, name='addstock'),
+    url(r'^transfers/$', views.viewtransfers, name='viewtransfers'),  
+    url(r'^transfers_form/$', views.addtransfers, name='addtransfers')
 ]
