fe77569d5cc7f536683834689d1b0d8f988041e4
Abhinandan Gaur <abhiabhigaur524@gmail.com>
2023-04-10 23:48:12 +0530

    fix models.py indentation

--- FusionIIIT/applications/scholarships/models.py
+++ FusionIIIT/applications/scholarships/models.py
@@ -8,57 +8,63 @@ from applications.globals.models import ExtraInfo
 
 class Constants:
     STATUS_CHOICES = (
-        ("Complete", "COMPLETE"),
-        ("Incomplete", "INCOMPLETE"),
-        ("Reject", "REJECT"),
-        ("Accept", "ACCEPT"),
+        ('Complete', 'COMPLETE'),
+        ('Incomplete', 'INCOMPLETE'),
+        ('Reject', 'REJECT'),
+        ('Accept', 'ACCEPT'),
     )
     TIME = (
-        ("0", "12 Midnight"),
-        ("1am", "1"),
-        ("2am", "2"),
-        ("3am", "3"),
-        ("4am", "4"),
-        ("5am", "5"),
-        ("6am", "6"),
-        ("7am", "7"),
-        ("8am", "8"),
-        ("9am", "9"),
-        ("10am", "10"),
-        ("11am", "11"),
-        ("12 Noon", "12"),
-        ("1pm", "13"),
-        ("2pm", "14"),
-        ("3pm", "15"),
-        ("4pm", "16"),
-        ("5pm", "17"),
-        ("6pm", "18"),
-        ("7pm", "19"),
-        ("8pm", "20"),
-        ("9pm", "21"),
-        ("10pm", "22"),
-        ("11pm", "23"),
-        ("12 Midnight", "0"),
+        ('0', '12 Midnight'),
+        ('1am', '1'),
+        ('2am', '2'),
+        ('3am', '3'),
+        ('4am', '4'),
+        ('5am', '5'),
+        ('6am', '6'),
+        ('7am', '7'),
+        ('8am', '8'),
+        ('9am', '9'),
+        ('10am', '10'),
+        ('11am', '11'),
+        ('12 Noon', '12'),
+        ('1pm', '13'),
+        ('2pm', '14'),
+        ('3pm', '15'),
+        ('4pm', '16'),
+        ('5pm', '17'),
+        ('6pm', '18'),
+        ('7pm', '19'),
+        ('8pm', '20'),
+        ('9pm', '21'),
+        ('10pm', '22'),
+        ('11pm', '23'),
+        ('12 Midnight', '0'),
     )
     BATCH = (
-        ("UG1", "UG1"),
-        ("UG2", "UG2"),
-        ("UG3", "UG3"),
-        ("UG4", "UG4"),
-        ("PG1", "PG1"),
-        ("PG2", "PG2"),
+        ('UG1', 'UG1'),
+        ('UG2', 'UG2'),
+        ('UG3', 'UG3'),
+        ('UG4', 'UG4'),
+        ('PG1', 'PG1'),
+        ('PG2', 'PG2'),
     )
     FATHER_OCC_CHOICE = (
-        ("government", "Government"),
-        ("private", "Private"),
-        ("public", "Public"),
-        ("business", "Business"),
-        ("medical", "Medical"),
-        ("consultant", "Consultant"),
-        ("pensioners", "Pensioners"),
+        ('government', 'Government'),
+        ('private', 'Private'),
+        ('public', 'Public'),
+        ('business', 'Business'),
+        ('medical', 'Medical'),
+        ('consultant', 'Consultant'),
+        ('pensioners', 'Pensioners'),
     )
-    MOTHER_OCC_CHOICES = (("EMPLOYED", "EMPLOYED"), ("HOUSE_WIFE", "HOUSE_WIFE"))
-    HOUSE_TYPE_CHOICES = (("RENTED", "RENTED"), ("OWNED", "OWNED"))
+    MOTHER_OCC_CHOICES = (
+        ('EMPLOYED', 'EMPLOYED'),
+        ('HOUSE_WIFE', 'HOUSE_WIFE')
+    )
+    HOUSE_TYPE_CHOICES = (
+        ('RENTED', 'RENTED'),
+        ('OWNED', 'OWNED')
+    )
 
     PROGRAMME_CHOICES = (
         ('B', 'UNDER_GRAD'),
@@ -75,11 +81,11 @@ class Constants:
 
 
 class Award_and_scholarship(models.Model):
-    award_name = models.CharField(max_length=100, default="")
+    award_name = models.CharField(max_length=100, default='')
     catalog = models.TextField(max_length=5000)
 
     class Meta:
-        db_table = "Award_and_scholarship"
+        db_table = 'Award_and_scholarship'
 
     def __str__(self):
         return self.award_name
@@ -93,12 +99,12 @@ class Mcm(models.Model):
     income_father = models.IntegerField(default=0)
     income_mother = models.IntegerField(default=0)
     income_other = models.IntegerField(default=0)
-    father_occ = models.CharField(
-        max_length=10, choices=Constants.FATHER_OCC_CHOICE, default=""
-    )
-    mother_occ = models.CharField(
-        max_length=10, choices=Constants.MOTHER_OCC_CHOICES, default=""
-    )
+    father_occ = models.CharField(max_length=10,
+                                  choices=Constants.FATHER_OCC_CHOICE,
+                                  default='')
+    mother_occ = models.CharField(max_length=10,
+                                  choices=Constants.MOTHER_OCC_CHOICES,
+                                  default='')
     father_occ_desc = models.CharField(max_length=30, null=True)
     mother_occ_desc = models.CharField(max_length=30, null=True)
     four_wheeler = models.IntegerField(blank=True, null=True)
@@ -116,20 +122,15 @@ class Mcm(models.Model):
     college_name = models.CharField(max_length=30, null=True)
     income_certificate = models.FileField(null=True, blank=True)
     forms = models.FileField(null=True, blank=True)
-    status = models.CharField(
-        max_length=10, choices=Constants.STATUS_CHOICES, default="INCOMPLETE"
-    )
-    student = models.ForeignKey(
-        Student, on_delete=models.CASCADE, related_name="mcm_info"
-    )
+    status = models.CharField(max_length=10, choices=Constants.STATUS_CHOICES, default='INCOMPLETE')
+    student = models.ForeignKey(Student,
+                                on_delete=models.CASCADE, related_name='mcm_info')
     annual_income = models.IntegerField(default=0)
     date = models.DateField(default=datetime.date.today)
-    award_id = models.ForeignKey(
-        Award_and_scholarship, default=4, on_delete=models.CASCADE
-    )
+    award_id = models.ForeignKey(Award_and_scholarship, default=4, on_delete=models.CASCADE)
 
     class Meta:
-        db_table = "Mcm"
+        db_table = 'Mcm'
 
     def __str__(self):
         return str(self.student)
@@ -139,37 +140,35 @@ class Notional_prize(models.Model):
     spi = models.FloatField()
     cpi = models.FloatField()
     year = models.CharField(max_length=10, choices=Constants.BATCH)
-    award_id = models.ForeignKey(
-        Award_and_scholarship, default=4, on_delete=models.CASCADE
-    )
+    award_id = models.ForeignKey(Award_and_scholarship, default=4, on_delete=models.CASCADE)
 
     class Meta:
-        db_table = "Notional_prize"
+        db_table = 'Notional_prize'
 
 
 # Addition: a column programme added
 class Previous_winner(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
-    programme = models.CharField(max_length=10, default="B.Tech")
+    programme = models.CharField(max_length=10, default='B.Tech')
     year = models.IntegerField(default=datetime.datetime.now().year)
     award_id = models.ForeignKey(Award_and_scholarship, on_delete=models.CASCADE)
 
     class Meta:
-        db_table = "Previous_winner"
+        db_table = 'Previous_winner'
 
 
 class Release(models.Model):
     date_time = models.DateTimeField(default=datetime.datetime.now, blank=True)
-    programme = models.CharField(max_length=10, default="B.Tech")
+    programme = models.CharField(max_length=10, default='B.Tech')
     startdate = models.DateField(default=datetime.date.today)
     enddate = models.DateField()
-    award = models.CharField(default="", max_length=50)
-    remarks = models.TextField(max_length=500, default="")
-    batch = models.TextField(default="all")
+    award = models.CharField(default='', max_length=50)
+    remarks = models.TextField(max_length=500, default='')
+    batch = models.TextField(default='all')
     notif_visible = models.IntegerField(default=1)
 
     class Meta:
-        db_table = "Release"
+        db_table = 'Release'
 
 
 # new class added for keeping track of notifications and applied application by students
@@ -185,7 +184,7 @@ class Notification(models.Model):
         return str(self.student_id)
 
     class Meta:
-        db_table = "Notification"
+        db_table = 'Notification'
 
 
 class Application(models.Model):
@@ -198,15 +197,13 @@ class Application(models.Model):
         return str(self.application_id)
 
     class Meta:
-        db_table = "Application"
+        db_table = 'Application'
 
 
 class Director_silver(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     award_id = models.ForeignKey(Award_and_scholarship, on_delete=models.CASCADE)
-    status = models.CharField(
-        max_length=10, choices=Constants.STATUS_CHOICES, default="INCOMPLETE"
-    )
+    status = models.CharField(max_length=10, choices=Constants.STATUS_CHOICES, default='INCOMPLETE')
     date = models.DateField(default=datetime.date.today)
     cultural_intercollege_certificate = models.FileField(blank=True, null=True)
     cultural_intercollege_certificates_no = models.IntegerField(blank=True, null=True)
@@ -243,14 +240,14 @@ class Director_silver(models.Model):
     sports_other_accomplishment_doc = models.FileField(null=True, blank=True)
 
     class Meta:
-        db_table = "Director_silver"
+        db_table = 'Director_silver'
 
 
 class DM_Proficiency_gold(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     award_id = models.ForeignKey(Award_and_scholarship, on_delete=models.CASCADE)
     status = models.CharField(
-        max_length=10, choices=Constants.STATUS_CHOICES, default="INCOMPLETE"
+        max_length=10, choices=Constants.STATUS_CHOICES, default='INCOMPLETE'
     )
     date = models.DateField(default=datetime.date.today)
     brief_description_project = models.TextField(max_length=1000, null=True)
@@ -286,13 +283,13 @@ class DM_Proficiency_gold(models.Model):
     workshop_doc = models.FileField(null=True, blank=True)  # new attribute added
 
     class Meta:
-        db_table = "DM_Proficiency_gold"
+        db_table = 'DM_Proficiency_gold'
 
 
 class Director_gold(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     status = models.CharField(
-        max_length=10, choices=Constants.STATUS_CHOICES, default="INCOMPLETE"
+        max_length=10, choices=Constants.STATUS_CHOICES, default='INCOMPLETE'
     )
     date = models.DateField(default=datetime.date.today)
     award_id = models.ForeignKey(
@@ -346,13 +343,13 @@ class Director_gold(models.Model):
     blood_donation = models.FileField(null=True, blank=True)  # new attribute added
 
     class Meta:
-        db_table = "Director_gold"
+        db_table = 'Director_gold'
 
 
 class IIITDM_Proficiency(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     status = models.CharField(
-        max_length=10, choices=Constants.STATUS_CHOICES, default="INCOMPLETE"
+        max_length=10, choices=Constants.STATUS_CHOICES, default='INCOMPLETE'
     )
     date = models.DateField(default=datetime.date.today)
     award_id = models.ForeignKey(
@@ -406,4 +403,4 @@ class IIITDM_Proficiency(models.Model):
     project_write_up = models.TextField(max_length=1000, null=True)
 
     class Meta:
-        db_table = "IIITDM_Proficiency"
+        db_table = 'IIITDM_Proficiency'
