878b1f621ae11e8c5694c4b1a25003302570fa82
Devendra <patildevendra07072002@gmail.com>
2023-04-16 17:45:24 +0530

    now showing alert and success messages

--- FusionIIIT/applications/eis/views.py
+++ FusionIIIT/applications/eis/views.py
@@ -12,7 +12,7 @@ from django.urls import reverse_lazy
 from django.views import generic
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from xhtml2pdf import pisa
-
+from django.contrib import messages
 from applications.eis import admin
 from applications.globals.models import ExtraInfo, HoldsDesignation, DepartmentInfo
 from django.http.response import JsonResponse
@@ -1368,8 +1368,8 @@ def consult_insert(request):
 def patent_insert(request):
     user = get_object_or_404(ExtraInfo, user=request.user)
     if(int(request.POST.get('earnings')) < 0):
-        message = "Earnings cannot be less than 0"
-        return redirect('/profile/?page6=1', {message : message})
+        messages.error(request, 'Earnings cannot be negative')
+        return HttpResponseRedirect('/profile')
     pf = request.user.id
 
     if (request.POST.get('patent_id')==None or request.POST.get('patent_id')==""):
@@ -1385,8 +1385,8 @@ def patent_insert(request):
     eis.status = request.POST.get('status')
     eis.a_month = request.POST.get('month')
     eis.save()
-    message = "New Patent created successfully"
-    return redirect('/profile/?page6=1' ,{message:message})
+    messages.success(request, 'New patent created')
+    return HttpResponseRedirect('/profile')
 
 def transfer_insert(request):
     user = get_object_or_404(ExtraInfo, user=request.user)
