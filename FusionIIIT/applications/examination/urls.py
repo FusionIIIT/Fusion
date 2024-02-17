db82ceefa924c2176cb72b374f775396800d26bf
harshabonu <152063196+harshabonu@users.noreply.github.com>
2024-02-17 22:27:29 +0530

    latest Examination Module (#1291)

--- FusionIIIT/applications/examination/urls.py
+++ FusionIIIT/applications/examination/urls.py
@@ -4,8 +4,16 @@ from django.urls import path, include
 from . import views
 from django.contrib import admin
 
-
+app_name = 'examination'
 
 urlpatterns = [
-    url(r'^api/', include('applications.examination.api.urls'))
+    url(r'^api/', include('applications.examination.api.urls')),
+    url(r'^$', views.exam, name='exam'),
+    url(r'submit/', views.submit, name='submit'),
+    url(r'verify/', views.verify, name='verify'),
+    url(r'publish/', views.publish, name='publish'),
+    url(r'notReady_publish/', views.notReady_publish, name='notReady_publish'),
+    url(r'announcement/', views.announcement, name='announcement'),
+    url(r'timetable/', views.timetable, name='timetable'),
+
 ]
