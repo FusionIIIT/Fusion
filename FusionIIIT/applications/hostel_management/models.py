651681c23fc39d8733f3976425d44a461d032fc2
Anuj Patel <anujkpatel4@gmail.com>
2024-02-20 22:55:40 +0530

    updated frontend using templates

--- FusionIIIT/applications/hostel_management/models.py
+++ FusionIIIT/applications/hostel_management/models.py
@@ -300,7 +300,7 @@ class StudentDetails(models.Model):
     address = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
-      return self.first_name + ' ' + self.last_name
+      return self.first_name
 
 
 
