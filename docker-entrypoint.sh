e97983b18863315004c18e34bcc955e01fb77024
Rushikesh Shinde <97105800+rush1kesh-010@users.noreply.github.com>
2024-02-23 19:46:02 +0530

    ac-6 : week-5 : enable notification, fix docker issues (#1309)
    * feat: enable dashboard department button, optimize code
    
    * ac-6 : week-5: create api (#1294)
    
    * feat: create api
    
    * feat:create more routes
    
    * feat : enable notification, fix docker issues
    
    ---------
    
    Co-authored-by: Aryan Shrivastava <107790721+aryan0723@users.noreply.github.com>
    Co-authored-by: Prajjwal kapoor <68912239+prajjwalkapoor@users.noreply.github.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
+
+# Apply database migrations
 echo "Apply database migrations"
-python FusionIIIT/manage.py makemigrations &&
-python FusionIIIT/manage.py migrate &&
+# python FusionIIIT/manage.py makemigrations
+# python FusionIIIT/manage.py migrate
+
+# Start server
 echo "Starting server"
-python FusionIIIT/manage.py runserver 
+python FusionIIIT/manage.py runserver 0.0.0.0:8000
