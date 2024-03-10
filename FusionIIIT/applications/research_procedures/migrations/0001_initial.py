18de59cd0ea720753c4ed739849a42fe99e674d3
SharanKumar06 <sharan123852@gmail.com>
2024-03-11 05:04:42 +0530

    added new usecases

--- FusionIIIT/applications/research_procedures/migrations/0001_initial.py
+++ FusionIIIT/applications/research_procedures/migrations/0001_initial.py
@@ -57,19 +57,19 @@ class Migration(migrations.Migration):
         #         ('students_under_group', models.ManyToManyField(related_name='allstudents', to=settings.AUTH_USER_MODEL)),
         #     ],
         # ),
-        migrations.CreateModel(
-            name='Patent',
-            fields=[
-                ('application_id', models.AutoField(primary_key=True, serialize=False)),
-                ('title', models.CharField(max_length=120)),
-                ('ipd_form', models.FileField(blank=True, null=True, upload_to='')),
-                ('project_details', models.FileField(blank=True, null=True, upload_to='')),
-                ('ipd_form_file', models.TextField(blank=True, null=True)),
-                ('project_details_file', models.TextField(blank=True, null=True)),
-                ('status', models.CharField(choices=[('Approved', 'Approved'), ('Disapproved', 'Disapproved'), ('Pending', 'Pending')], default='Pending', max_length=20)),
-                ('faculty_id', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='globals.extrainfo')),
-            ],
-        ),
+        # migrations.CreateModel(
+        #     name='Patent',
+        #     fields=[
+        #         ('application_id', models.AutoField(primary_key=True, serialize=False)),
+        #         ('title', models.CharField(max_length=120)),
+        #         ('ipd_form', models.FileField(blank=True, null=True, upload_to='')),
+        #         ('project_details', models.FileField(blank=True, null=True, upload_to='')),
+        #         ('ipd_form_file', models.TextField(blank=True, null=True)),
+        #         ('project_details_file', models.TextField(blank=True, null=True)),
+        #         ('status', models.CharField(choices=[('Approved', 'Approved'), ('Disapproved', 'Disapproved'), ('Pending', 'Pending')], default='Pending', max_length=20)),
+        #         ('faculty_id', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='globals.extrainfo')),
+        #     ],
+        # ),
         # migrations.CreateModel(
         #     name='ConsultancyProject',
         #     fields=[
