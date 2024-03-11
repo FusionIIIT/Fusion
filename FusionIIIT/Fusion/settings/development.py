729e03581aaa02f72937c966f4965312f344721a
Harshit0009 <tiwariharshit909@gmail.com>
2024-03-11 13:46:29 +0530

    changed development

--- FusionIIIT/Fusion/settings/development.py
+++ FusionIIIT/Fusion/settings/development.py
@@ -18,7 +18,6 @@ DATABASES = {
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
