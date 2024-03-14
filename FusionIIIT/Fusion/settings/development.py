5df74651dd5655135b42e3f5e242dc1920321ace
Harshul-25 <charshul25@gmail.com>
2024-03-14 12:21:28 +0530

    tested crontab

--- FusionIIIT/Fusion/settings/development.py
+++ FusionIIIT/Fusion/settings/development.py
@@ -34,6 +34,7 @@ if DEBUG:
     INSTALLED_APPS += (
         'debug_toolbar',
         'django_extensions',
+        'django_crontab',
         )
 
 
@@ -67,3 +68,10 @@ CRONTAB_DJANGO_MANAGE_PATH = '/home/owlman/Desktop/Fuse/Fusion/FusionIIIT/manage
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT=os.path.join(BASE_DIR,"media/")
+
+CRONJOBS = [
+    ('* * * * *', 'applications.central_mess.tasks.my_scheduled_task'),
+    # Add more cron jobs as needed
+]
+
+CRONTAB_DJANGO_MANAGE_PATH = '/home/bankai/Desktop/virtual_env/Fusion/FusionIIIT/manage.py'
