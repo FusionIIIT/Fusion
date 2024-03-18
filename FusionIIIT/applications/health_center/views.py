aff778cdc617a6bf3af4b59e2a6d61df2544166d
Prem Charan <charan30052004@gmail.com>
2024-03-18 14:59:34 +0530

    Revert "integrated dashboard"
    This reverts commit 8e48ec598072104d1678bfede3daab6fb8d20a2f.

--- FusionIIIT/applications/health_center/views.py
+++ FusionIIIT/applications/health_center/views.py
@@ -38,7 +38,7 @@ def healthcenter(request):
     usertype = ExtraInfo.objects.select_related('user','department').get(user=request.user).user_type
     print(usertype)
 
-    if usertype == 'student' or usertype=='faculty' or usertype=='staff':
+    if usertype == 'student' or usertype=='fx' or usertype=='staff':
         return HttpResponseRedirect("/healthcenter/student")
     elif usertype == 'compounder':
         return HttpResponseRedirect("/healthcenter/compounder")
@@ -143,7 +143,7 @@ def student_view(request):
     '''                                                                 # student view starts here
     usertype = ExtraInfo.objects.select_related('user','department').get(user=request.user).user_type
     print("yes")
-    if usertype == 'student' or usertype == 'faculty' or usertype == 'staff':
+    if usertype == 'student' or usertype == 'fx' or usertype == 'staff':
         if request.method == 'POST':
             return student_view_handler(request)
 
