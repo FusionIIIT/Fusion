920dcce8fbd2f6413c7c963e5283c3a69d399e83
nidhibarapatre <116302859+nidhibarapatre@users.noreply.github.com>
2024-03-26 19:52:14 +0530

    Gad-3 : fixed minor issues (#1389)
    * GAD-5: Fix responsiveness (#1125)
    
    Co-authored-by: A Anunaya <76819712+Anunaya07@users.noreply.github.com>
    Co-authored-by: Aksh Bansal <63552235+Aksh-Bansal-dev@users.noreply.github.com>
    
    * Minor changes
    
    * Implementation of FTS and completion of the redirect functionality
    
    * Change in model of supervisor
    
    * Complition of redirect functionality and pagination
    
    * Fixation of a error
    
    * Completion of module
    
    * GAD -5 Dashboard Changes (#1325)
    
    * dashboard and usercard updated (#1329)
    
    * Fixing ui bugs (#1335)
    
    * Integrated dashborad
    
    * creating new branch
    
    * disallowing migrations
    
    * removing migration files
    
    * resolved errors
    
    ---------
    
    Co-authored-by: Harshvardhan Singh <73544247+Lawful2002@users.noreply.github.com>
    Co-authored-by: A Anunaya <76819712+Anunaya07@users.noreply.github.com>
    Co-authored-by: Aksh Bansal <63552235+Aksh-Bansal-dev@users.noreply.github.com>
    Co-authored-by: akshatnema <20bcs022@iiitdmj.ac.in>
    Co-authored-by: BlackHAWK2001 <150950834+BlackHAWK2001@users.noreply.github.com>
    Co-authored-by: Arpit Tak <140220904+Ishu-ji@users.noreply.github.com>
    Co-authored-by: SukulSarve <143542658+SukulSarve@users.noreply.github.com>

--- FusionIIIT/applications/complaint_system/urls.py
+++ FusionIIIT/applications/complaint_system/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     url(r'^caretaker/$', views.caretaker, name='caretaker'),
     url(r'^caretaker/feedback/(?P<feedcomp_id>[0-9]+)/$', views.feedback_care),
     url(r'^caretaker/pending/(?P<cid>[0-9]+)/$', views.resolvepending),
-    url(r'^caretaker/detail/(?P<detailcomp_id1>[0-9]+)/$', views.detail),
+    url(r'^caretaker/detail2/(?P<detailcomp_id1>[0-9]+)/$', views.detail),
     url(r'^caretaker/search_complaint$', views.search_complaint),
     url(r'^caretaker/(?P<complaint_id>[0-9]+)/feedback/$', views.submitfeedbackcaretaker),
     
@@ -29,8 +29,8 @@ urlpatterns = [
     url(r'^supervisor/$', views.supervisor),
     url(r'^supervisor/feedback/(?P<feedcomp_id>[0-9]+)/$', views.feedback_super),
     url(r'^supervisor/caretaker_id_know_more/(?P<caretaker_id>[0-9]+)/$', views.caretaker_id_know_more),
-    url(r'^supervisor/caretaker_id_know_more/(?P<caretaker_id>[0-9]+)/complaint_reassign_super/(?P<iid>[0-9]+)/$', views.complaint_reassign_super, name = 'complaint_reassign_super'),
-    url(r'^supervisor/detail3/(?P<detailcomp_id1>[0-9]+)/$', views.detail3, name = 'detail3'),
+    # url(r'^supervisor/caretaker_id_know_more/(?P<caretaker_id>[0-9]+)/complaint_reassign_super/(?P<iid>[0-9]+)/$', views.complaint_reassign_super, name = 'complaint_reassign_super'),
+    url(r'^supervisor/detail/(?P<detailcomp_id1>[0-9]+)/$', views.detail3, name = 'detail3'),
     url(r'^supervisor/pending/(?P<cid>[0-9]+)/$', views.resolvependingsuper),
     url(r'^supervisor/(?P<complaint_id>[0-9]+)/$', views.submitfeedbacksuper),
     
