734e48cc23031e74c1fb042cae781a0c4bf28ee1
Abhikumar Gupta <97209802+abhi87374@users.noreply.github.com>
2024-04-09 16:45:21 +0530

    Revert "merging Os-3 into staging-2 (#1404)" (#1405)
    This reverts commit eb9ec489c30cbd261dcfc21c247f00a123f066b1.

--- FusionIIIT/applications/globals/api/urls.py
+++ FusionIIIT/applications/globals/api/urls.py
@@ -19,4 +19,3 @@ urlpatterns = [
 
 
 ]
-
--- FusionIIIT/applications/programme_curriculum/models.py
+++ FusionIIIT/applications/programme_curriculum/models.py
@@ -186,6 +186,7 @@ class Semester(models.Model):
 class Course(models.Model):
     '''
         Current Purpose : To store the details regarding a course
+         
 
         
         
@@ -338,6 +339,7 @@ class CourseInstructor(models.Model):
       class Meta:
           unique_together = ('course_id', 'instructor_id', 'batch_id')
       
+
 
       def __self__(self):
             return '{} - {}'.format(self.course_id, self.instructor_id)
