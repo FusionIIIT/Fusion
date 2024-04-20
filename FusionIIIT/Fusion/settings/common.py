c59c83600232f698c573f99463ab1c0eea477fcf
Prabhat Suman <32027224+prabhatsuman@users.noreply.github.com>
2024-04-20 23:00:36 +0530

    Update common.py
    removed duplicate Media_Root

--- FusionIIIT/Fusion/settings/common.py
+++ FusionIIIT/Fusion/settings/common.py
@@ -260,7 +260,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_EMAIL_REQUIRED = True
