5ff28797581a008e99b590a96ca686f3b2575063
SharanKumar06 <sharan123852@gmail.com>
2024-04-25 15:33:28 +0530

    multi copis

--- FusionIIIT/applications/research_procedures/models.py
+++ FusionIIIT/applications/research_procedures/models.py
@@ -101,34 +101,15 @@ class requests(models.Model):
     class Meta:
         ordering = ['-request_id']
     
-# class requests(models.Model):
-#     request_id=models.IntegerField(primary_key=True)
-#     project_id= models.ForeignKey(projects, on_delete=models.CASCADE)
-#     request_type=models.CharField(max_length=500)
-#     project_investigator_id=models.ForeignKey(User, related_name='rj_pi'  , on_delete= models.CASCADE)
-#     status= models.IntegerField(default=0) #value 0 means pending
-#     description=models.CharField(max_length=400,default=None, null= True)
-#     amount= models.IntegerField(default=0) #value 0 means pending
+class co_project_investigator(models.Model):
+    co_pi_id= models.ForeignKey(User, on_delete=models.CASCADE)
+    project_id= models.ForeignKey(projects, on_delete=models.CASCADE)
 
-#     class Meta:
-#         ordering = ['-request_id']
+    def __str__(self):
+        return str(self.co_pi_id)
     
-# class rspc_inventory(models.Model):
-#     inventory_id=models.IntegerField(primary_key=True)
-#     project_id= models.ForeignKey(projects, on_delete=models.CASCADE)
-#     project_investigator_id=models.ForeignKey(User, related_name="rin_pi" , on_delete= models.CASCADE)
-#     status= models.IntegerField(default=0) #value 0 means pending
-#     description=models.CharField(max_length=400)
-#     amount= models.IntegerField(default=0) #value 0 means pending
-
-# class project_staff_info(models.Model):
-#     staff_id=models.CharField(primary_key=True,max_length=400)
-#     project_investigator_id= models.ForeignKey(User, on_delete=models.CASCADE)
-#     project_id=models.ForeignKey(projects, related_name='p_pji', on_delete=models.CASCADE)
-#     staff_name=models.CharField(max_length=400)
-#     status=models.IntegerField(default=0)
-#     description=models.CharField(max_length=400)
-
+    class Meta:
+        ordering = ['-co_pi_id']
     
     
     
