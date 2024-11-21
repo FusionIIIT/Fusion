ba657442d1e7004f1b88caa8164460ba8f4df18e
Kushagra <22bcs148@iiitdmj.ac.in>
2024-11-21 17:35:10 +0530

    adds the submit marks and view attendance routes

--- FusionIIIT/applications/online_cms/api/urls.py
+++ FusionIIIT/applications/online_cms/api/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<course_code>[A-Za-z0-9]+)/(?P<version>[\d.]+)/$', views.course, name='course'),
-    url(r'^courses', views.courseview, name='courseview'),
+    url(r'^(?P<course_code>[A-Za-z0-9]+)/(?P<version>[\d.]+)', views.course, name='course'),
+    url(r'^courses', views.course, name='courseview'),
+    url(r'^attendance', views.view_attendance, name='view_attendance'),
+    url(r'^(?P<course_code>[A-z0-9]+)/(?P<version>[\d.]+)/submit_marks$',views.submit_marks, name='submit_marks')
 ]
