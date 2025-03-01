674f3b260fc4b27f18cb47f6b08eed56930d91df
ramGopal <129493911+ramG-reddy@users.noreply.github.com>
2025-03-01 23:17:46 +0530

    [New-UI] Backend changes required for React frontend (#1713)
    * New year migrations
    
    * Changes for React-UI

--- FusionIIIT/applications/globals/api/urls.py
+++ FusionIIIT/applications/globals/api/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
 
     url(r'^auth/login/', views.login, name='login-api'),
     url(r'^auth/logout/', views.logout, name='logout-api'),
+    url(r'^auth/me', views.auth_view, name='auth-api'),
+    url(r'^update-role/', views.update_last_selected_role, name='update_last_selected_role'),
     # generic profile endpoint
     #code of corresponding view is modifiedtemporary because of mismatched designations
     url(r'^profile/(?P<username>.+)/', views.profile, name='profile-api'),
@@ -14,9 +16,8 @@ urlpatterns = [
     url(r'^profile_update/', views.profile_update, name='update-profile-api'),
     url(r'^profile_delete/(?P<id>[0-9]+)/', views.profile_delete, name='delete-profile-api'),
 
-    url(r'^dashboard/',views.dashboard,name='dashboard-api'),
     url(r'^notification/',views.notification,name='notification'),
-    url(r'^notification/read',views.NotificationRead,name='notifications-read')
-
-
+    url(r'^notificationread',views.NotificationRead,name='notifications-read'),
+    url(r'^notificationdelete',views.delete_notification,name='notifications-delete'),
+    url(r'^notificationunread',views.NotificationUnread,name='notifications-unread')
 ]
