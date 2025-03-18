3f34917f9cae45f745b38c14cd4492dafed4a42e
Divyansh Bisht <115489768+dcoder13@users.noreply.github.com>
2025-03-19 01:18:43 +0530

    Gad 2 (Request based system) (#1726)
    * Added API folder in iwdModuleV2
    
    * Added endpoints in API folder in iwdModuleV2
    
    * Added additional endpoints in API folder in iwdModuleV2
    
    * fix[api] : fetchdesignation, requestview
    
    * fix : view file endpoint
    
    * minor updates while updating the issue-work-order table (#4)
    
    * minor changes (#5)
    
    * minor changes
    
    * minor changes
    
    * approve/reject  and view file
    
    * fix: work order
    
    * Minor changes in addBudget and editBudget endpoints (#6)
    
    * fix : serializer
    
    * modified views related to audit document view (#7)
    
    * Added API folder in iwdModuleV2
    
    * modified views related to audit document view
    
    * fixed apis
    
    * fix: serializer overriding requestCreatedBdBy value
    
    * fix: serializer overriding requestCreatedBdBy value
    
    * fix api
    
    * chore: made handlers consistent
    
    * major fixes
    
    * fix: handle dean process request file attachment upload
    
    * fix : forward file
    
    * fix:migrations
    
    ---------
    
    Co-authored-by: Bhargavzz <vishnubhargavsitra@gmail.com>
    Co-authored-by: Abhijeet <pabhijeet349@gmail.com>
    Co-authored-by: dotniiko <115548274+dotniiko@users.noreply.github.com>
    Co-authored-by: Dhruv Ratmele <129955117+Somgester@users.noreply.github.com>
    Co-authored-by: Abhijeet Patil <133658873+abhiJeetP10@users.noreply.github.com>
    Co-authored-by: Bhargavzz <133690080+Bhargavzz@users.noreply.github.com>

---
+++ FusionIIIT/applications/iwdModuleV2/api/serializers.py
@@ -0,0 +1,140 @@
+from rest_framework import serializers
+from applications.globals.models import *
+from applications.iwdModuleV2.models import *
+from applications.ps1.models import *
+
+class WorkOrderFormSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WorkOrder
+        fields = '__all__'
+
+class AgreementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Agreement
+        fields = '__all__'
+
+class MilestonesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Milestones
+        fields = '__all__'
+
+class ExtensionOfTimeDetailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ExtensionOfTimeDetails
+        fields = '__all__'
+
+class ProjectsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Projects
+        fields = '__all__'
+
+class DesignationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Designation
+        fields = ['id', 'name']
+
+class HoldsDesignationSerializer(serializers.ModelSerializer):
+    designation = DesignationSerializer()
+    username = serializers.CharField(source='user.username')
+
+    class Meta:
+        model = HoldsDesignation
+        fields = ['id', 'designation', 'username']
+
+class CreateRequestsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Requests
+        fields = ['id', 'name', 'area', 'description', 'requestCreatedBy']
+
+    def create(self, validated_data):
+        validated_data['engineerProcessed'] = 0
+        validated_data['directorApproval'] = 0
+        validated_data['deanProcessed'] = 0
+        validated_data['status'] = "Pending"
+        validated_data['issuedWorkOrder'] = 0
+        validated_data['workCompleted'] = 0
+        validated_data['billGenerated'] = 0
+        validated_data['billProcessed'] = 0
+        validated_data['billSettled'] = 0
+        return super().create(validated_data)
+    
+class DirectorApprovedRequestsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Requests
+        fields = ['id', 'name', 'area', 'description', 'requestCreatedBy']
+
+class WorkUnderProgressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Requests
+        fields = ['id', 'name', 'area', 'description', 'requestCreatedBy', 'issuedWorkOrder', 'workCompleted']
+
+
+class RequestsInProgressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Requests
+        fields = ['id', 'name', 'area', 'description', 'requestCreatedBy', 'issuedWorkOrder', 'workCompleted']
+
+class PageOneDetailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PageOneDetails
+        fields = '__all__'
+
+class PageTwoDetailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PageTwoDetails
+        fields = '__all__'
+
+class PageThreeDetailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PageThreeDetails
+        fields = '__all__'
+
+class AESDetailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AESDetails
+        fields = '__all__'
+
+class CorrigendumTableSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CorrigendumTable
+        fields = '__all__'
+
+class AddendumSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Addendum
+        fields = '__all__'
+
+class PreBidDetailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PreBidDetails
+        fields = '__all__'
+
+class NoOfTechnicalBidTimesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NoOfTechnicalBidTimes
+        fields = '__all__'
+
+class TechnicalBidDetailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TechnicalBidDetails
+        fields = '__all__'
+
+class TechnicalBidContractorDetailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TechnicalBidContractorDetails
+        fields = '__all__'
+
+class FinancialBidDetailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FinancialBidDetails
+        fields = '__all__'
+
+class FinancialContractorDetailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FinancialContractorDetails
+        fields = '__all__'
+
+class LetterOfIntentDetailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LetterOfIntentDetails
+        fields = '__all__'
