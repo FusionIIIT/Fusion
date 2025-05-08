87e1e366314fcbce1dd2c2df1bb85b14ec3e70eb
Zaid Alam <predcodes695@gmail.com>
2025-05-08 22:25:07 +0530

    fix: minor changes in File Model

--- FusionIIIT/applications/filetracking/migrations/0003_auto_20250508_1905.py
+++ FusionIIIT/applications/filetracking/migrations/0003_auto_20250508_1905.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('filetracking', '0002_auto_20250508_1903'),
+        ('filetracking', '0001_initial'),
     ]
 
     operations = [
